7edf770b4672b7579d8ebf7a694b4358fcf615e8
cat <cattttttslmao@gmail.com>
2021-06-24 20:10:37 -0500

    [Lobby] New Discord Invite

--- gmtlobby/entities/entities/gmt_donationsign/cl_init.lua
+++ gmtlobby/entities/entities/gmt_donationsign/cl_init.lua
@@ -1,31 +1,49 @@
 
 -----------------------------------------------------
-include('shared.lua')
-
-ENT.RenderGroup = RENDERGROUP_BOTH
-
-function ENT:Draw()
-	self:DrawModel()
-end
-
-function ENT:Think()
+include('shared.lua')
+
+
+
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+function ENT:Draw()
+	self:DrawModel()
+end
+
+
+
+function ENT:Think()
+
 
 	self:SetSkin( 2 )
 
-	/*if LocalPlayer().IsVIP and LocalPlayer():IsVIP() then
-		self:SetSkin( 2 )
-	else
-		self:SetSkin( 1 )
-	end*/
-
-end
-
+	/*if LocalPlayer().IsVIP and LocalPlayer():IsVIP() then
+
+		self:SetSkin( 2 )
+
+	else
+
+		self:SetSkin( 1 )
+
+	end
+*/
+
+
+end
+
+
+
 net.Receive( "OpenDiscord", function()
-
-	local URL = "https://discord.gg/ehBpEnE"
-	local Title = "GMTower: Deluxe - Discord"
-
-	--browser.OpenURL( URL, Title )
+
+
+	local URL = "https://discord.gg/CYJG7paNH9"
+	local Title = "GMTower: Deluxe - Discord"
+
+
+
+	--browser.OpenURL( URL, Title )
+
 	gui.OpenURL( URL )
-
+
+
 end )
