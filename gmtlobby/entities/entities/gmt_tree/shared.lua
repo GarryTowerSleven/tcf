81c92128e71427dc487d9bf20f0ff67839e1af24
Anomaladox <exsoldierstrife@gmail.com>
2021-06-26 23:43:00 -0400

    [Lobby] Slight Tree Animation Edit

--- gmtlobby/entities/entities/gmt_tree/shared.lua
+++ gmtlobby/entities/entities/gmt_tree/shared.lua
@@ -11,30 +11,33 @@ ENT.PrintName	= "Palm Tree"
 ENT.Model = Model("models/map_detail/foliage/coconut_tree_01.mdl")
 ENT.LODModel = Model("models/map_detail/foliage/coconut_tree_01_lod.mdl")
 
-//ENT.PalmSeq = "wind_light"
+ENT.PalmSeq = "wind_light"
+ENT.AnimStarted = false
 
-//function ENT:Initialize()
-//    if math.random(1,2) == 1 then self.PalmSeq = "wind_light_b" end
-//end
+function ENT:Initialize()
+	if ( CLIENT ) then
+		if math.random(1,2) == 1 then self.PalmSeq = "wind_light_b" end
+	end
+end
 
 function ENT:Think()
-	if ( SERVER ) then
-		//self:NextThink( CurTime() )
-    //if self:GetModel() == self.Model then
-    //  self:ResetSequence( self.PalmSeq )
-    //end
-		return true
-  end
 
-  if CLIENT then
-    if ( !LocalPlayer():GetPos():WithinDistance( self:GetPos(), 2048 ) ) then
-      if self:GetModel() == self.LODModel then return end
-      self:SetModel( self.LODModel )
-    else
-      if self:GetModel() == self.Model then return end
-      self:SetModel( self.Model )
-      //self:ResetSequence( self.PalmSeq )
-    end
-  end
+	if ( SERVER && !self.AnimStarted ) then
+		self.AnimStarted = true
+		if self:GetModel() == self.Model then
+			self:ResetSequence( self.PalmSeq )
+		end
+	end
+
+	if ( CLIENT ) then
+		if ( LocalPlayer():GetPos():Distance( self:GetPos() ) <= 2048 ) then
+			self:SetSequence( self.PalmSeq )
+		else
+			self:SetSequence( 0 )
+		end
+	end
+
+	self:NextThink( CurTime() )
+	return true
 
 end
