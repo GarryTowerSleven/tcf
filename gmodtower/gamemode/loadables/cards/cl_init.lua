bdc2c361d6f0708e46ee659f0e6b8bf93813068c
AmGona <44752650+AmGona@users.noreply.github.com>
2021-07-11 20:38:07 -0700

    [Lobby] Lobby 1 Casino HUD

--- gmodtower/gamemode/loadables/cards/cl_init.lua
+++ gmodtower/gamemode/loadables/cards/cl_init.lua
@@ -16,7 +16,7 @@ include( "sh_player.lua" )
 
 module( "Cards", package.seeall )
 
-
+local chipsMat = Material( "gmod_tower/icons/chip.png" )
 
 hook.Add( "HUDPaint", "DrawChips", function()
 
@@ -30,7 +30,23 @@ hook.Add( "HUDPaint", "DrawChips", function()
 
 
 
-	GTowerHUD.DrawExtraInfo( GTowerIcons2.GetIcon("chips"), " " .. tostring(chips), 16 )
+	if GTowerHUD.Style:GetInt() == 2 then
+		chips = "CHIPS: " .. string.FormatNumber( chips )
+
+		local off = 10
+
+		surface.SetMaterial( chipsMat )
+		surface.SetDrawColor( 50, 50, 50 )
+		surface.DrawTexturedRect( 100, ScrH() - 115 + off, 32, 32 )
+		surface.SetDrawColor( 100, 100, 100 )
+		surface.DrawTexturedRect( 80, ScrH() - 120 + off, 32, 32 )
+		surface.SetDrawColor( 255, 255, 255 )
+		surface.DrawTexturedRect( 90, ScrH() - 125 + off, 32, 32 )
+
+		draw.SimpleShadowText( chips, "GTowerHUDMain", 125, ScrH() - 113 + off, Color( 255, 255, 255, 255 ), color_black, TEXT_ALIGN_LEFT )
+	else
+		GTowerHUD.DrawExtraInfo( GTowerIcons2.GetIcon("chips"), " " .. tostring(chips), 16 )
+	end
 
 
 
