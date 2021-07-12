fb2ad0e2ca79d29b11c06627c338645a5522499e
cat <cattttttslmao@gmail.com>
2021-07-11 20:39:26 -0500

    [Lobby] Add "contentmanager"

--- gmtlobby/gamemode/shared.lua
+++ gmtlobby/gamemode/shared.lua
@@ -48,6 +48,7 @@ Loadables.Load( {
 	"legs",									-- First person legs
 	"fakeclientmodel",			-- Fake player models for use such as the monorail
 	"question",					-- Global voting
+	"contentmanager",			-- Missing Content Notification
 
 	-- Items Depend On These
 	---------------------------------------------------------
