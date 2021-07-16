fd2c4212437298f05de1903c4fb5856651f70821
Anomaladox <exsoldierstrife@gmail.com>
2021-07-15 23:12:20 -0400

    [Global] voice enable replicated

--- gmodtower/gamemode/shared.lua
+++ gmodtower/gamemode/shared.lua
@@ -25,12 +25,17 @@ end
 
 IsLobby = IsLobby()
 
+-- Voice Enable Replacement
 if CLIENT then
 	CreateConVar( "gmt_voice_enable", 1, { FCVAR_USERINFO, FCVAR_ARCHIVE } )
 else
 	hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
-		if !tobool(listener:GetInfoNum( "gmt_voice_enable", 1 )) then return false end
+		if VoiceNotEnabled(listener) || VoiceNotEnabled(talker) then return false end
 	end )
+end
+
+function VoiceNotEnabled(ply)
+	return !tobool(ply:GetInfoNum( "gmt_voice_enable", 1 ))
 end
 
 //=====================================================
