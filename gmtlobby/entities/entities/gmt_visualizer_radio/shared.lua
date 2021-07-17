b2e66394b2beedb1ff56ac9b44b55fd795d6460c
cat <cattttttslmao@gmail.com>
2021-07-17 18:17:03 -0500

    [Lobby] Use Prompt for Radio

--- gmtlobby/entities/entities/gmt_visualizer_radio/shared.lua
+++ gmtlobby/entities/entities/gmt_visualizer_radio/shared.lua
@@ -1,3 +1,7 @@
 ENT.Base			= "mediaplayer_visualizer"
 ENT.Type			= "anim"
 ENT.Model			= Model( "models/props/cs_office/radio.mdl")
+
+function ENT:CanUse( ply )
+	return true, "REQUEST MUSIC"
+end
