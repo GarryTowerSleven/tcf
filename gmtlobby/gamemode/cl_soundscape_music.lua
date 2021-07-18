66d07ba4a6fd65fffed7c26bf133f7bdeb123aa7
cat <cattttttslmao@gmail.com>
2021-07-18 13:50:16 -0500

    [Lobby] 1:1 Soundscapes

--- gmtlobby/gamemode/cl_soundscape_music.lua
+++ gmtlobby/gamemode/cl_soundscape_music.lua
@@ -1,22 +1,20 @@
-
------------------------------------------------------
 local function isEmpty(str)
 	return str == nil or str == ""
 end
 
-local lastRand
-local curRand
-
+local lastRand 
+local curRand 
 local function uniqueRandom(min, max )
+
 	-- Prevent infinite loop
 	if min == max then return min end
 
-	-- Repeat until we have a new unique number
+	-- Repeat until we have a new unique number 
 	repeat
 		curRand = math.random(min, max)
 	until (curRand ~= lastRand )
 
-	return curRand
+	return curRand 
 end
 
 local Enabled = CreateClientConVar("gmt_bgmusic_enable", "1", true, false )
@@ -36,42 +34,38 @@ cvars.AddChangeCallback(Volume:GetName(), function(name, old, new )
 end )
 
 -- Set initial volume
-
 soundscape.SetVolume("music", math.Clamp(Volume:GetInt(), 0, 100) / 100)
 
 -- Some locations call for certain behavior of soundscapes, more than just groups
 -- This function is fine-tuned for background music, not just generic soundscapes
 -- To define a custom musicscape, name it exactly as you would a normal soundscape, prefixed with "music_"
 -- For example, a custom musicscape for the "lobby" location would be named "music_lobby"
-
 soundscape = soundscape or {}
-
 function soundscape.GetMusicSoundscape(loc)
-	--local location = Location.Get(loc)
-	--if not location then return end
-
-	if loc >= 1 && loc <= 13 then
-		return "music_condos"
-	end
-
-	if Location.GetGroup(loc) == nil then return end
+	local location = Location.Get(loc)
+	if not location then return end
 
 	-- First, see if there's a soundscape defined for the current specific location
-	local scape = soundscape.IsDefined("music_" .. Location.GetGroup(loc)) and "music_" .. Location.GetGroup(loc) or nil
-	scape = scape and string.lower(scape) or nil
+	local scape = soundscape.IsDefined("music_" .. location.Name) and "music_" .. location.Name or nil 
+	scape = scape and string.lower(scape) or nil 
 
 	-- if it's registered, return
 	if not isEmpty(scape) then return scape end
 
 	-- Move on to any overrides before we get to a 'group' soundscape
 	-- Play a super quiet soundscape when they're in the movie theater itself
-	if Location.GetGroup(loc) == "theater" and Location.GetFriendlyName(loc) ~= "theatermain" then
+	if location.Group == "theater" and location.Name ~= "theatermain" then
 		return "music_theater_inside"
 	end
 
 	-- When in the stores, stop playing the plaza soundscape
-	if Location.GetGroup(loc) == "stores" and Location.GetFriendlyName(loc) ~= "stores" then
+	if location.Group == "stores" and location.Name ~= "stores" then
 		return "music_stores_inside"
+	end
+
+	-- Fix for the condos
+	if location.CondoID then
+		return "music_condo"
 	end
 
 	-- Just use default methods to find the soundscape
@@ -79,34 +73,24 @@ function soundscape.GetMusicSoundscape(loc)
 
 	-- Return what we've got
 	return (scape and soundscape.IsDefined("music_" .. scape) ) and string.lower("music_" .. scape) or nil
-
 end
 
 -- Very similar to the one in cl_soundscape.lua
 -- However, this one changes PASSIVELY, so that it's only changed when it's set to change
 -- This keeps the music going even in new locations, whereas the other does not
-
 hook.Add("Location", "MusicscapeChangeLocation", function(ply, loc)
-
-	if ply != LocalPlayer() then return end
 	if not Enabled:GetBool() then return end
 
 	-- Retrieve the two locations
-	local gLoc = Location.GetGroup(loc)
-
-	if gLoc == nil then gLoc = "" end
-
-	local newGroup = string.lower( gLoc )
+	local newGroup = string.lower(Location.GetGroup(loc))
 
 	-- Get the soundscape matching this location
 	local sndscape = soundscape.GetMusicSoundscape(loc)
 
 	-- if there's no soundscapes for this location stop the presses
 	if isEmpty(sndscape) or not soundscape.IsDefined(sndscape) then
-
-		--print(soundscape.IsPlaying("music_global_ambient"))
 		 -- Only stop the soundscape if they're in no man's land
-		if loc == 1 or ply:Location() == 1 then
+		if loc == 0 or Location.Get(loc) == nil then
 			soundscape.StopChannel("music")
 
 		-- Just play an ambient music track if there's no music override here
@@ -125,149 +109,80 @@ hook.Add("Location", "MusicscapeChangeLocation", function(ply, loc)
 end )
 
 
-
 --------------------------------------
-
 --      SOUNDSCAPE DEFINITIONS      --
-
 -- TODO: Better place to put these? --
-
 --------------------------------------
-
 
 local LobbyMusicName = "GModTower/soundscapes/music/lobby"
-
 local LobbyMusicCount = 10 -- Define the number of music files for ambient lobby jams
-
 local LobbySongs = {}
-
 for n=1, LobbyMusicCount do 
-
 	table.insert(LobbySongs, {LobbyMusicName .. n .. ".mp3", 10} )
-
 end
-
 soundscape.Register("music_global_ambient", 
-
 {
-
 	-- Tell the soundscape system that when this is usually removed and faded out, keep it alive
-
 	idle = true, 
 
-
-
 	-- Select a random song to play every once in a while
-
 	{
-
 	type = "playlist",
-
 		time = {60 * 0.5, 60 * 5}, -- Play the next song 0.5 to 5 minutes after the song ends
 
-
-
 		-- Override the sound selector function with our own
-
 		sounds = LobbySongs,
-
 	},
-
 })
-
-
 
 -- Dynamically create the soundscapes based on gamemode names
+for _, v in pairs(Location.Locations) do
+	local gmode = GTowerServers:GetGamemode( v.Name )
+	if not gmode then continue end 
 
-for k, v in pairs(Location.Locations) do
-
-
-	local name = Location.GetGroup(k)
-
-	local gmode = GTowerServers:GetGamemode( name )
-
-	if not gmode then continue end
-
-
-
-	-- Format the songs so they're in { song, length } format
-
+	-- Format the songs so they're in { song, length } format 
 	local formattedSongs = {}
-
 	for __, song in pairs(gmode.Music) do
-
 		table.insert(formattedSongs, {song, 10})
-
 	end
 
-
-
 	-- Create the rule to insert into the soundscape system
-
-	print("Registering soundscape", name)
-
-	soundscape.Register("music_" .. name,
-
-	{
-
+	print("Registering soundscape", v.Name)
+	soundscape.Register("music_" .. v.Name, 
+	{ 
 		dsp = 0,
 
-
-
 		{
-
 		type = "playrandom_bass",
-
 			volume = 0.70,
-
 			sounds = formattedSongs,
-
 		},
-
 	})
-
 end
 
-
-
 soundscape.Register("music_lobby", 
-
 {
-
 	{
-
 	type = "playlooping",
-
 		-- Limit the volume
-
 		volume = 0.25,
 
-
-
 		-- Start this sound's position syncronized with servertime
-
 		--sync = true,
 
-
-
 		-- All sounds are in a table format of {soundpath, soundlength}
-
 		sound = {Sound("GModTower/soundscapes/music/towermainlobby1.mp3"), 234},
-
 	},
-
 })
 
-
-
 -- Mute BG music in elevator
-soundscape.Register("music_elevators", {})
+soundscape.Register("music_elevator", {})
 
 -- Mute any music in the theater
 soundscape.Register("music_theater_inside", {} )
 
 -- Mute any music in the condo
-soundscape.Register("music_condos", {})
+soundscape.Register("music_condo", {})
 
 -- Mute any music in the nightclub
 soundscape.Register("music_nightclub", {})
@@ -277,6 +192,3 @@ soundscape.Register("music_duels", {})
 
 -- Mute any music in the duels arena
 soundscape.Register("music_duelarena", {})
-
--- Mute any music in somewhere
-soundscape.Register("music_somewhere", {})
