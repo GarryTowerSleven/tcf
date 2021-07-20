ed39cb15774a4cbfabd58d6a739d0cfc31a718c3
cat <cattttttslmao@gmail.com>
2021-07-19 19:33:50 -0500

    [Lobby] Halloween Ghost Location Check

--- gmtlobby/entities/entities/ghost_ghost/init.lua
+++ gmtlobby/entities/entities/ghost_ghost/init.lua
@@ -14,3 +14,13 @@ ENT.SAttack = { "room209/ghost/attack", 3 }
 ENT.SDie = { "room209/ghost/death", 3 }
 ENT.SIdle = { "room209/ghost/idle", 5 }
 ENT.SPain = { "room209/ghost/pain", 3 }
+
+local function LobbyCheck()
+    for k,v in pairs(ents.FindByClass("ghost_ghost")) do
+        if !Location.IsGroup( v:Location(), "lobby" ) then
+            v:SetPos( halloween2014.CenterPos )
+        end
+    end
+end
+
+hook.Add( "Think", "StayInLobbyBitch", LobbyCheck )
