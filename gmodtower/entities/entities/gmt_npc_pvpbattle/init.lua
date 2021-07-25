41aa6b5773a3ee73b02a5d8245c68e2fd012b3c2
Anomaladox <exsoldierstrife@gmail.com>
2021-07-25 05:32:31 -0400

    [Lobby] Another small NPC edit

--- gmodtower/entities/entities/gmt_npc_pvpbattle/init.lua
+++ gmodtower/entities/entities/gmt_npc_pvpbattle/init.lua
@@ -1,3 +1,14 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include('shared.lua')
+
+function ENT:AcceptInput( name, activator, ply )
+
+    if name == "Use" && ply:IsPlayer() && ply:KeyDownLast(IN_USE) == false then
+		
+		timer.Simple( 0.0, function()
+			PvpBattle.OpenStore( PvpBattle, ply )
+		end )
+		
+    end 
+end
