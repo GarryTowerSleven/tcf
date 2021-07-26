d9ac80d168f4eea05946c4583f9f29398b0bdd1d
AmGona <44752650+AmGona@users.noreply.github.com>
2021-07-25 22:19:57 -0700

    [Source Karts] Shield is now accurate

--- sourcekarts/entities/entities/sk_item_shield/init.lua
+++ sourcekarts/entities/entities/sk_item_shield/init.lua
@@ -6,11 +6,15 @@ include("shared.lua")
 function ENT:Initialize()
   self:SetModel(self.Model)
   self:DrawShadow(false)
-
+  self:SetMaterial("models/wireframe")
+  self:SetColor(Color(200,200,255,255))
   self:GetOwner():GetKart():SetIsInvincible(true)
+  self.ShieldSound = CreateSound( self, self.Sound )
+  self.ShieldSound:Play()
 
 end
 
 function ENT:OnRemove()
     self:GetOwner():GetKart():SetIsInvincible(false)
+    self.ShieldSound:Stop()
 end
