dd3ab9a2ba2c3cb41e1d271f228e1c74d5bb5d7c
AmGona <44752650+AmGona@users.noreply.github.com>
2021-12-21 21:24:27 -0800

    [Ball Race] Potential missing ball fix

--- ballrace/entities/entities/player_ball/init.lua
+++ ballrace/entities/entities/player_ball/init.lua
@@ -25,7 +25,7 @@ function ENT:Initialize()
 	self:SphereInit(self.radius)
 
 	if ply.ModelSet == nil then
-		self:SetModel( self.Model )
+		self:SetModel( "models/gmod_tower/BALL.mdl" )
 	else
 		self:SetModel( ply.ModelSet )
 	end
