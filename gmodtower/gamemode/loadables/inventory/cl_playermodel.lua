0c5a2e2479822fdf511b34d1e2e6b2b72d28cda9
AmGona <44752650+AmGona@users.noreply.github.com>
2021-12-30 14:54:44 -0800

    [Lobby] Playermodel Tooltips

--- gmodtower/gamemode/loadables/inventory/cl_playermodel.lua
+++ gmodtower/gamemode/loadables/inventory/cl_playermodel.lua
@@ -1,20 +1,20 @@
-hook.Add("ClientExtraModels", "InventoryCheck", function( func )
+hook.Add("ClientExtraModels", "InventoryCheck", function( func, clickfunc, category )
 
 	local function TestForItem( ItemObj )
-		if ItemObj.Item && ItemObj.Item:IsValid() && ItemObj.Item.ModelItem == true then
-			func( ItemObj.Item.ModelName .. "-" .. ItemObj.Item.ModelSkinId, ItemObj.Item.Model, ItemObj.Item.ModelSkinId )
+		if ItemObj.Item && ItemObj.Item:IsValid() && ItemObj.Item.ModelItem == true then			
+			func( ItemObj.Item.ModelName .. "-" .. ItemObj.Item.ModelSkinId, ItemObj.Item.Model, ItemObj.Item.ModelSkinId, clickfunc, category, 0, 0, ItemObj.Item.Description, ItemObj.Item.Name )
+			//func( ItemObj.Item.ModelName .. "-" .. ItemObj.Item.ModelSkinId, ItemObj.Item.Model, ItemObj.Item.ModelSkinId )
 		end
 	end
-
-	for _, tbl in pairs( GTowerItems.Items ) do
-		TestForItem( tbl )
-		/*for _, v in pairs( tbl ) do
-			PrintTable(tbl)
+	
+	for _, tbl in pairs( GTowerItems.ClientItems ) do
+		
+		for _, v in pairs( tbl ) do
 			TestForItem( v )
-		end*/
-
+		end
+		
 	end
-
+	
 end )
 
 hook.Add("InventoryChanged", "RecreatePlayerList", function( ItemObj )
