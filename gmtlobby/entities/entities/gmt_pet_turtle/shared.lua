520ef91568611be6a7289982d09bda243d1a2a04
Cat <cattttttslmao@gmail.com>
2021-12-30 23:06:26 -0600

    [Global] Move fuckton of entities to gmtlobby

--- gmtlobby/entities/entities/gmt_pet_turtle/shared.lua
+++ gmtlobby/entities/entities/gmt_pet_turtle/shared.lua
@@ -1,25 +1,6 @@
 
------------------------------------------------------
-ENT.Type 			= "anim"
+ENT.Type = "anim"
+ENT.Category = "GMTower"
 
-ENT.Base 			= "base_anim"
-
-ENT.PrintName		= "Pet Flying Turtle"
-
-
-
-ENT.Spawnable		= false
-
-ENT.AdminSpawnable	= false
-
-
-
-ENT.AutomaticFrameAdvance = true
-
-ENT.Model			= "models/gmod_tower/plush_turtle.mdl"
-
-function ENT:SetupDataTables()
-
-	self:NetworkVar( "String", 0, "PetName" )
-
-end
+ENT.PrintName = "Flying Turtle Pet"
+ENT.Spawnable = true
