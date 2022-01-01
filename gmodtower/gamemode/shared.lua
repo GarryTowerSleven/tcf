3a7dff7832babb6565f25b2795642f771267a1d5
Cat <cattttttslmao@gmail.com>
2022-01-01 14:20:24 -0600

    [GModTower] Initial shared changes

--- gmodtower/gamemode/shared.lua
+++ gmodtower/gamemode/shared.lua
@@ -1,13 +1,17 @@
----------------------------------
 GM.Name     = "GMod Tower"
-GM.Author   = "GTower Team"
-GM.Email    = ""
-GM.Website  = ""
-GM.ContentPrefix = "../addons/gmodtower/"
-GM.DownloadsEnabled = false
+GM.Author   = "Deluxe Team & PixelTail Games"
+GM.Website  = "https://www.gmtdeluxe.org/"
+GM.WebsiteUrl  = "https://www.gmodtower.org/apps/" // TODO: Change to gmtdeluxe.org
 
 co_color = Color( 50, 255, 50 )
 co_color2 = Color( 255, 50, 50 )
+
+// This breaks things????
+--DeriveGamemode("base")
+
+GMT = true
+TestingMode = CreateConVar( "gmt_testing", 0, { FCVAR_ARCHIVE, FCVAR_DONTRECORD, FCVAR_REPLICATED }, "GMT testing mode" )
+EmptyFunction = function() end
 
 //=====================================================
 function IsLobby()
