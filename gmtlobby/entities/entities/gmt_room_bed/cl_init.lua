77f584eed558fa466bb28d3b89a4eae97483ce3d
Anomaladox <exsoldierstrife@gmail.com>
2022-01-15 14:22:35 -0500

    Funneh Funneh Bed

--- gmtlobby/entities/entities/gmt_room_bed/cl_init.lua
+++ gmtlobby/entities/entities/gmt_room_bed/cl_init.lua
@@ -4,3 +4,35 @@ include("shared.lua")
 function ENT:Draw()
 	self.Entity:DrawModel()
 end
+
+surface.CreateFont( "SleepText", {
+	font = "KoorkinW04-BoldItalic",
+	extended = false,
+	size = 45,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
+hook.Add( "GTowerHUDPaint", "SleepyTimeMessage", function()
+	if LocalPlayer().Sleeping == true then
+		draw.DrawText( LocalPlayer().SleepMessage, "SleepText", math.fmod(RealTime() * 15,ScrW()*0.5), math.fmod(RealTime() * 5,ScrH()*0.25), color_white, TEXT_ALIGN_CENTER )
+	end
+end )
+
+net.Receive( "BedMessage", function( len, ply )
+	local message = net.ReadString()
+	local sleeping = net.ReadBool()
+	
+	LocalPlayer().Sleeping = sleeping
+	LocalPlayer().SleepMessage = message
+end )
