389394a52b77f9fe466e4a376188736c8a5b71c6
Cat <cattttttslmao@gmail.com>
2022-01-16 14:02:09 -0600

    [Global] Disable workshop temporarily, will be replaced eventually with Deluxe's

--- gmodtower/gamemode/init.lua
+++ gmodtower/gamemode/init.lua
@@ -18,7 +18,7 @@ resource.AddWorkshop( 104548572 ) -- Playable piano
 resource.AddWorkshop( 546392647 ) -- Media player
 
 // Lobby 2 content from before shutdown, hidden
-resource.AddWorkshop( 2667443678 ) -- base
+/*resource.AddWorkshop( 2667443678 ) -- base
 resource.AddWorkshop( 2667447617 ) -- lobby
 resource.AddWorkshop( 2667452517 ) -- lobby2
 resource.AddWorkshop( 2667461993 ) -- ballrace
@@ -27,7 +27,7 @@ resource.AddWorkshop( 2667466895 ) -- virus
 resource.AddWorkshop( 2667468743 ) -- chimera
 resource.AddWorkshop( 2667470886 ) -- minigolf
 resource.AddWorkshop( 2667474570 ) -- zombiemassacre
-resource.AddWorkshop( 2667477578 ) -- karts
+resource.AddWorkshop( 2667477578 ) -- karts*/
 
 MultiUsers = {}
 
