3391cf72085e279b34483e175a07eeda3c063f44
Anomaladox <exsoldierstrife@gmail.com>
2022-01-26 00:29:59 -0500

    Revert "Update init.lua"
    This reverts commit b49d31f38adc08c4c5dff8ed0e2d5d6370d46cb4.

--- PVPBattle/gamemode/init.lua
+++ PVPBattle/gamemode/init.lua
@@ -89,9 +89,6 @@ function GM:PlayerSpawn( ply )
 			music.Play( 1, 1, ply )
 		end)
 	end
-	
-	// Indexes Player Weapons
-	ply.PlayerWeapons = PvpBattle:GiveWeapons( ply )
 
 	// Stop observer mode
  	ply:UnSpectate()
@@ -116,10 +113,7 @@ function GM:PlayerSpawn( ply )
 		ply._TheKid = 0
 	end
 
-<<<<<<< HEAD
 	self:PlayerLoadout( ply )
-=======
->>>>>>> parent of 0388bcd (Revert "[PVPBattle] I SMRT")
 	self:PlayerResetSpeed( ply )
 end
 
@@ -129,7 +123,7 @@ function GM:PlayerResetSpeed( ply )
 end
 
 function GM:PlayerLoadout( ply )
-	if self.GiveAllWeapons == true  || ply:IsBot() then
+	if self.GiveAllWeapons == true || !PvpBattle || ply:IsBot() then
 		for _, v in ipairs( self.Weapons ) do
 			ply:Give( v )
 		end
