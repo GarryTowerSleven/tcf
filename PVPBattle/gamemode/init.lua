a35fc15c6cf7883d4cd47d97f7891c5d7473f339
Anomaladox <exsoldierstrife@gmail.com>
2022-01-26 18:37:28 -0500

    [PVPBattle] Fug yaself

--- PVPBattle/gamemode/init.lua
+++ PVPBattle/gamemode/init.lua
@@ -159,6 +159,16 @@ function PopulateLoadout( ply, delay )
 		if IsValid( ply ) then
 			if ply.WeaponList == nil then
 				ply.WeaponList = PvpBattle:GiveWeapons( ply )
+
+				if ply.WeaponList == {} then
+					ply.WeaponList = {
+						"weapon_toyhammer",
+						"weapon_bouncynade",
+						"weapon_semiauto",
+						"weapon_supershotty",
+						"weapon_thompson"
+					}
+				end
 			end
 
 			for k,v in pairs( ply.WeaponList ) do
