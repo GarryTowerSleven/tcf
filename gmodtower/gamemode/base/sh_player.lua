565986765f53c7e9db65043630bb0dc239fb1322
Cat <cattttttslmao@gmail.com>
2022-01-29 15:20:12 -0600

    [Global] Muffin Mod

--- gmodtower/gamemode/base/sh_player.lua
+++ gmodtower/gamemode/base/sh_player.lua
@@ -14,6 +14,8 @@ local Roles =
 	{ "STEAM_0:1:57386100", "Moderator" },  // Squibbus
 	{ "STEAM_0:1:85508734", "Moderator" },	// Breezy
 	{ "STEAM_0:0:156132358", "Moderator" },	// Basical
+	{ "STEAM_0:1:72402171", "Moderator" },	// Umbre
+	{ "STEAM_0:0:63281019", "Moderator" },	// Muffin
 
 	// Current PT Staff
 	{ "STEAM_0:1:6044247", "PixelTail" },	// MacDGuy
