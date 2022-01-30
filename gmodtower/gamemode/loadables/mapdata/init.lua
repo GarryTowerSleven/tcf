487501b4c9f9e4dd8ef53c0723fbca81f5ca4de3
Cat <cattttttslmao@gmail.com>
2022-01-29 23:07:09 -0600

    [Lobby] Skybox fog improvement

--- gmodtower/gamemode/loadables/mapdata/init.lua
+++ gmodtower/gamemode/loadables/mapdata/init.lua
@@ -1,11 +1,12 @@
 AddCSLuaFile("cl_init.lua")
-include "functions.lua"
+include("functions.lua")
 
+// server
 local mapdata = "gamemodes/gmodtower/gamemode/loadables/mapdata/maps/" .. game.GetMap() .. ".lua"
 
 if file.Exists( mapdata, "GAME" ) then
 	MsgC( co_color, "[MapData] Loading MapData for ".. game.GetMap() .."\n")
     include( "maps/"..game.GetMap()..".lua" )
 else
-	MsgC( co_color2, "[MapData] No MapData found for " .. game.GetMap() .. "!\n")
+	MsgC( co_color2, "[MapData] No MapData found for " .. game.GetMap() .. ".\n")
 end
