13738d56d552143bfd8bb3730a8fc90949f03aca
AmGona <44752650+AmGona@users.noreply.github.com>
2022-02-05 23:21:55 -0800

    what the fuck why did mysql commit

--- gmodtower/gamemode/base/database/mysql.lua
+++ gmodtower/gamemode/base/database/mysql.lua
@@ -1,8 +1,8 @@
 // Database Details
-local host = "localhost"
-local user = "root"
-local password = "amgonasqlpass"
-local database = "gmtlobbydatadeluxe"
+local host = ""
+local user = ""
+local password = ""
+local database = ""
 
 local os, arch, mysqlooModulePath, mysqlooExists, sqlType
 
