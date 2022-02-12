a70853386d86d627cbc71875bb1f7a0aaef060db
Anomaladox <exsoldierstrife@gmail.com>
2022-02-12 14:59:04 -0500

    [General] Cleanup and Coolness

--- gmtgamemode/gamemode/modules/music/init.lua
+++ gmtgamemode/gamemode/modules/music/init.lua
@@ -1,5 +1,3 @@
-
------------------------------------------------------
 include( "sh_init.lua" )
 AddCSLuaFile( "sh_init.lua" )
 AddCSLuaFile( "cl_init.lua" )
@@ -10,39 +8,39 @@ module( "music", package.seeall )
 
 function Play( event, id, ply )
 
-  /*
-  EVENT_PLAY
-  EVENT_STOP
-  EVENT_VOLUME
-  */
+	/*
+	EVENT_PLAY
+	EVENT_STOP
+	EVENT_VOLUME
+	*/
 
-  if !id then return end
+	if !id then return end
 
-  net.Start("MusicEvent")
-  net.WriteInt( tonumber(event) , 4 )
-  net.WriteInt( tonumber(id) , 8 )
+	net.Start("MusicEvent")
+	net.WriteInt( tonumber(event) , 4 )
+	net.WriteInt( tonumber(id) , 8 )
 
-  if IsValid(ply) then
-    net.Send(ply)
-  else
-    net.Broadcast()
-  end
+	if IsValid(ply) then
+		net.Send(ply)
+	else
+		net.Broadcast()
+	end
 
 end
 
 function StopAll( fade, time )
 
-  net.Start("MusicEvent")
-  net.WriteInt( 3 , 4 )
+	net.Start("MusicEvent")
+	net.WriteInt( 3 , 4 )
 
-  if fade then
-    net.WriteBool( fade )
-  end
+	if fade then
+		net.WriteBool( fade )
+	end
 
-  if time then
-    net.WriteInt( time, 8 )
-  end
+	if time then
+		net.WriteInt( time, 8 )
+	end
 
-  net.Broadcast()
+	net.Broadcast()
 
 end
