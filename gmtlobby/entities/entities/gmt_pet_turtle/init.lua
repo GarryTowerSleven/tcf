795f341007f4f5b96b40782d680fff7a437b4b41
AmGona <44752650+AmGona@users.noreply.github.com>
2022-05-08 00:09:28 -0700

    [Lobby] Correct turtle fix

--- gmtlobby/entities/entities/gmt_pet_turtle/init.lua
+++ gmtlobby/entities/entities/gmt_pet_turtle/init.lua
@@ -46,6 +46,13 @@ function ENT:Initialize()
 
 end
 
+function ENT:UpdatePetName()
+	local ply = self:GetOwner()
+	if IsValid( ply ) then
+		self:SetPetName( string.sub(ply:GetInfo("gmt_petname_turtle"),1,15) )
+	end
+end
+
 function ENT:OnRemove()
 
 	--self:EmitRandomSound( "Deleted", 5 )
@@ -75,6 +82,83 @@ function ENT:Teleport( ply )
 	--self:EmitRandomSound( "Teleporting", 30 )
 end
 
+function ENT:EmitRandomSound( emotion, chance )
+
+	if ( chance && math.random( 1, chance ) != 1 ) then return end
+
+	local soundIndex = Pets.GetRandomSoundIndex( "melon", emotion )
+
+	if ( soundIndex == -1 ) then return end
+
+	local sound = Pets.GetSound( "melon", emotion, soundIndex )
+
+	self:EmitSound( sound, 80, 100 )
+end
+
+function ENT:Use( ply )
+
+end
+
+function ENT:Touch( ent )
+end
+
+function ENT:EmotionThink()
+
+	if self.NextEmotionThink >= CurTime() then return end
+
+	local owner = self:GetOwner()
+
+	if IsValid(owner) then self:UpdatePetName() end
+
+	if self.Emotion != Pets.GetIDFromEmotion( "Rolling" ) then
+		if ( IsValid( owner ) && owner:IsRabbit() ) then
+			self:SetEmotion( "Rabbit" )
+		else
+			self:SetEmotion( "Happy" )
+		end
+	end
+
+	local physObj = self:GetPhysicsObject()
+
+	if IsValid( physObj ) then
+		local velocity = physObj:GetVelocity():Length()
+
+		if ( velocity > 50 ) then
+			self:ResetIdle()
+		end
+
+		if ( velocity > self.RollingVelocity ) then
+			self:SetEmotion( "Rolling" )
+			self:ConfirmEmotion()
+			self.NextEmotionThink = CurTime() + 0.5
+
+			return
+		end
+
+	end
+
+	local actionDiff = self:IdleTime()
+
+	if ( actionDiff > 1 && actionDiff < 3 && self.Emotion == Pets.GetIDFromEmotion( "Rolling" ) ) then
+		self:SetEmotion( "Dizzy", 2 )
+	elseif ( actionDiff > self.DullTime ) then
+		self:SetEmotion( "Dull" )
+	elseif ( actionDiff > self.BoredTime ) then
+		self:SetEmotion( "Bored" )
+	end
+
+	local nearby = self:GetNearby()
+
+	if ( nearby == NEARBY_PET ) then
+		self:SetEmotion( "Wink" )
+	end
+
+	self:ConfirmEmotion()
+
+	self.NextEmotionThink = CurTime() + 0.5
+
+end
+
 function ENT:ResetIdle()
 	self.LastActionTime = CurTime()
 end
@@ -90,6 +174,8 @@ function ENT:Think()
 	if IsValid(self) then self:Remove() end
 	return
 	end
+
+	self:EmotionThink()
 
 end
 
@@ -135,6 +221,39 @@ function ENT:GetNearby()
 
 end
 
+function ENT:SetEmotion( emote, time )
+
+	if ( self.EmoteTime && self.EmoteTime > CurTime() ) then return end // another emotion is already playing!
+
+	self.TempEmotion = emote
+
+	self.EmoteTime = CurTime() + ( time or 0 )
+
+end
+
+function ENT:ConfirmEmotion( soundChance )
+
+	local tempId = Pets.GetIDFromEmotion( self.TempEmotion )
+
+	// short circuit
+	if ( self.Emotion == tempId ) then return end
+
+	self.Emotion = tempId
+
+	local randIndex = Pets.GetRandomQuoteIndex( "melon", self.Emotion )
+
+	if math.random( 1, 2 ) == 1 && randIndex != -1 then
+		umsg.Start( "PetMSG" )
+			umsg.Entity( self )
+			umsg.Char( self.Emotion )
+			umsg.Char( randIndex )
+		umsg.End()
+	end
+
+	--self:EmitRandomSound( self.Emotion, soundChance or 10 )
+
+end
+
 function ENT:PhysicsUpdate( phys )
 
 	local velocity = phys:GetVelocity()
@@ -154,3 +273,19 @@ function ENT:PhysicsUpdate( phys )
 	phys:ApplyForceCenter( vect )
 
 end
+
+concommand.Add( "gmt_petemote", function( ply, cmd, args )
+
+	if !IsValid( ply.Pet ) then return end
+
+	if #args == 0 then return end
+
+	local emote = tonumber( args[ 1 ] )
+	local time = tonumber( args[ 2 ] )
+
+	if !emote then return end
+
+	ply.Pet:SetEmotion( emote, time )
+	ply.Pet:ConfirmEmotion()
+
+end )
