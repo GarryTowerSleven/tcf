2574c1f70c6533262cb31d0648d42f8e6c9b6aa5
AmGona <44752650+AmGona@users.noreply.github.com>
2022-05-08 19:35:40 -0700

    [Lobby] gmt_bat thing

--- gmtlobby/entities/weapons/gmt_bat/shared.lua
+++ gmtlobby/entities/weapons/gmt_bat/shared.lua
@@ -37,8 +37,8 @@ function SWEP:PrimaryAttack()
 
 	if (ply.NextBat or 0) > CurTime() then return end
 
-	if ( Location.IsEquippablesNotAllowed( Location.Find(ply:GetPos()) ) ) then return end
-	if ( Location.Find(ply:GetPos()).CondoID ) then return end
+	if ( Location.IsEquippablesNotAllowed( ply:Location() ) ) then return end
+	if ( Location.IsCondo( ply:Location(), ply:Location() ) ) then return end
 
 	ply.NextBat = CurTime() + 5
 
