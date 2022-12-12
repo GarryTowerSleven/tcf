3f590c6f7acb58dc27dd9dc4da664b29fe7651e8
Anomaladox <exsoldierstrife@gmail.com>
2022-12-12 16:02:51 -0500

    [Lobby] Forgot to rename the instance of the gui

--- gamemodes/gmtlobby/entities/entities/gmt_game_checkers/cl_init.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_game_checkers/cl_init.lua
@@ -324,7 +324,7 @@ net.Receive( "checkersH", function( len )
 	CheckersHelp.Close = function()
 		CheckersHelp:Remove()
 		CheckersHelp = nil
-		GTowerMainGui:HideMenus()
+		GtowerMainGui:HideMenus()
 	end
 	
 	
