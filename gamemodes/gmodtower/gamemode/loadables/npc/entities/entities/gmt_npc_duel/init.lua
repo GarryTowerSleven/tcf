2a5ab4665981f643e151fb1942a1ceee2ffa12a5
LeadKiller <hl2epicfan@gmail.com>
2023-03-22 13:13:33 -0700

    [Lobby] Duel Store

--- gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_duel/init.lua
+++ gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_duel/init.lua
@@ -1,11 +1,3 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include('shared.lua')
-
-function ENT:AcceptInput( name, activator, ply )
-    if ( !self.NextUse ) then self.NextUse = 0 end
-    if CurTime() < self.NextUse then return end
-	self.NextUse = CurTime() + 1
-
-	ply:Msg2( T( "DuelsDisable" ) )
-end
