646ff0326f91c1851cc3f702fcd0c2de20a0f041
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-03-27 13:32:31 -0400

    [Barfight] Gah! So Stupid!

--- gamemodes/gmtlobby/entities/weapons/weapon_fist/shared.lua
+++ gamemodes/gmtlobby/entities/weapons/weapon_fist/shared.lua
@@ -4,3 +4,25 @@ SWEP.Primary.Damage		= {40, 60}
 if SERVER then
 	AddCSLuaFile( "shared.lua" )
 end
+
+function SWEP:PrimaryAttack()
+	if !self:CanPrimaryAttack() then return end
+	self.Weapon:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+
+	self:ShootMelee( self.Primary.Damage, self.FistHit, self.FistHitFlesh, self.FistMiss )
+end
+
+function SWEP:SecondaryAttack()
+end
+
+function SWEP:Reload()
+	return false
+end
+
+function SWEP:CanPrimaryAttack()
+	return true
+end
+
+function SWEP:CanSecondaryAttack()
+	return true
+end
