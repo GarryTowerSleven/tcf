ab9b1a1681d482729da5d9517bca587552cfab9b
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-03-27 11:41:59 -0700

    [Lobby] Fix emotes not being reset on death

--- gamemodes/gmodtower/gamemode/loadables/emote/shared.lua
+++ gamemodes/gmodtower/gamemode/loadables/emote/shared.lua
@@ -1,8 +1,20 @@
 --print('a')
 hook.Add( "Move", "EmoteMove", function( ply, mv ) 
 	
-	if ply:GetNWBool("Emoting") then
+	if IsEmoting(ply) then
 		return true
 	end
 	
 end)
+
+hook.Add( "PlayerDeath", "GlobalDeathMessage", function( victim, inflictor, attacker )
+	if victim:GetNWBool("Emoting") then
+		victim:SetNWBool("Emoting",false)
+	end
+end )
+
+function IsEmoting( ply )
+
+	return ply:GetNWBool("Emoting") --ply.EmoteID && ply.EmoteID > 0
+
+end
