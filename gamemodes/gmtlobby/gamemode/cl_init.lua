487b1f040ecf6f19b81b249ad80025ba95ea2612
LeadKiller <hl2epicfan@gmail.com>
2023-03-29 12:05:51 -0700

    [Lobby] Inventory Fix

--- gamemodes/gmtlobby/gamemode/cl_init.lua
+++ gamemodes/gmtlobby/gamemode/cl_init.lua
@@ -1,5 +1,45 @@
 include("shared.lua")
 
+// Create GMT fonts
+surface.CreateFont( "tiny", { font = "Arial", size = 10, weight = 100 } )
+surface.CreateFont( "smalltiny", { font = "Arial", size = 12, weight = 100 } )
+surface.CreateFont( "small", { font = "Arial", size = 14, weight = 400 } )
+surface.CreateFont( "smalltitle", { font = "Arial", size = 16, weight = 600 } )
+
+local mainFont = "CenterPrintText"
+surface.CreateFont( "GTowerhuge", { font = mainFont, size = 45, weight = 100 } )
+surface.CreateFont( "GTowerbig", { font = mainFont, size = 28, weight = 125 } )
+surface.CreateFont( "GTowerbigbold", { font = mainFont, size = 20, weight = 1200 } )
+surface.CreateFont( "GTowerbiglocation", { font = mainFont, size = 28, weight = 125 } )
+surface.CreateFont( "GTowermidbold", { font = mainFont, size = 16, weight = 1200 } )
+surface.CreateFont( "GTowerbold", { font = mainFont, size = 14, weight = 700 } )
+
+surface.CreateFont( "GTowersmall", { font = mainFont, size = 14, weight = 100 } )
+
+local mainFont2 = "Oswald"
+surface.CreateFont( "GTowerHUDHuge", { font = mainFont2, size = 50, weight = 400 } )
+surface.CreateFont( "GTowerHUDMainLarge", { font = mainFont2, size = 38, weight = 400 } )
+surface.CreateFont( "GTowerHUDMain", { font = mainFont2, size = 24, weight = 400 } )
+surface.CreateFont( "GTowerHUDMainMedium", { font = mainFont2, size = 20, weight = 400 } )
+surface.CreateFont( "GTowerHUDMainSmall", { font = mainFont2, size = 18, weight = 400 } )
+surface.CreateFont( "GTowerHUDMainSmall2", { font = "Clear Sans", size = 18, weight = 800 } )
+surface.CreateFont( "GTowerHUDMainTiny", { font = mainFont2, size = 16, weight = 400 } )
+surface.CreateFont( "GTowerHUDMainTiny2", { font = "Clear Sans", size = 12, weight = 400 } )
+surface.CreateFont( "GTowerNPC", { font = mainFont2, size = 100, weight = 800 } )
+
+surface.CreateFont( "GTowerHudCText", { font = "default", size = 35, weight = 700 } )
+surface.CreateFont( "GTowerHudCSubText", { font = "default", size = 18, weight = 700, } )
+surface.CreateFont( "GTowerHudCNoticeText", { font = "default", size = 16, weight = 700, } )
+surface.CreateFont( "GTowerHudCNewsText", { font = "default", size = 16, weight = 700, } )
+
+surface.CreateFont( "GTowerTabNotice", { font = "Impact", size = 30, weight = 400 } )
+surface.CreateFont( "GTowerMinigame", { font = "Impact", size = 24, weight = 400 } )
+surface.CreateFont( "GTowerGMTitle", { font = "Impact", size = 24, weight = 400 } )
+surface.CreateFont( "GTowerMessage", { font = "Arial", size = 16, weight = 600 } )
+surface.CreateFont( "GTowerToolTip", { font = "Tahoma", size = 16, weight = 400 } )
+
+//surface.CreateFont( "GTowerFuel", { font = "Impact", size = 18, weight = 400 } )
+
 inv = inv or {}
 
 net.Receive("Inventory", function()
@@ -65,7 +105,7 @@ for i = 0, #inv - 1 do
             surface.SetDrawColor(0, 0, 0, 100)
             --render.SetScissorRect( self.x + x, self.y + y, x + w, y + h, true )
             surface.DrawRect(x, y, w, h)
-            draw.SimpleText("ITEM", "DermaDefault", x, y, color_white)
+            draw.SimpleText("ITEM", "GTowerHUDMainTiny2", x, y, color_white)
         end
     end
 end
