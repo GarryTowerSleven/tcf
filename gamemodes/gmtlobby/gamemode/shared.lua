89805be584207aeb169fe1febc2ab45706745535
LeadKiller <hl2epicfan@gmail.com>
2023-03-29 11:22:43 -0700

    [Lobby] Disallow building outside condos

--- gamemodes/gmtlobby/gamemode/shared.lua
+++ gamemodes/gmtlobby/gamemode/shared.lua
@@ -1,1 +1,18 @@
 DeriveGamemode("darkrp")
+
+function InCondo(pos)
+    return pos.z > 11000
+end
+
+function GM:SpawnMenuOpen()
+    if !InCondo(LocalPlayer():GetPos()) then
+        notification.AddLegacy("Building is only allowed in Condos!", NOTIFY_ERROR, 8)
+        return false
+    end
+
+    return true
+end
+
+function GM:ContextMenuOpen()
+    return false
+end
