a7f327c93c8bb2e057fdc0ff42beb082395c0446
cattslmao <cattttttslmao@gmail.com>
2023-04-08 19:03:10 -0500

    [Lobby] Disable This for Now

--- gamemodes/gmodtower/gamemode/base/friends/init.lua
+++ gamemodes/gmodtower/gamemode/base/friends/init.lua
@@ -2,9 +2,9 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
-hook.Add( "PlayerCanHearPlayersVoice", "BlockedVoices", function( listener, talker )
+/*hook.Add( "PlayerCanHearPlayersVoice", "BlockedVoices", function( listener, talker )
     return not Friends.IsBlocked( listener, talker )
-end )
+end )*/
 
 net.Receive( "FriendStatus", function( len, ply )
 	local friends = net.ReadTable()
