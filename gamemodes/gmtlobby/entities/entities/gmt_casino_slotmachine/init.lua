93544d44e944e9945d818982cbc9acf95cbaaccd
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-04-13 10:17:56 -0400

    [Casino] Spinner Chance improvements

--- gamemodes/gmtlobby/entities/entities/gmt_casino_slotmachine/init.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_casino_slotmachine/init.lua
@@ -331,9 +331,11 @@ function ENT:PickResults()
 	local random = { getRand(), getRand(), getRand() }
 
 	if random[1] == 2 and random[2] == 2 and random[3] == 2 then
-		if math.random(20) != 1 && self:GetJackpot() < 50000 then
-			random[3] = math.random(3, 6)
-		else
+		if self:GetJackpot() < 10000 && math.random(20) != 1 then
+			random[3] = math.random(6)
+		elseif self:GetJackpot() < 25000 && math.random(10) != 1
+			random[3] = math.random(6)
+		elseif self:GetJackpot() < 50000 && math.random(5) != 1
 			random[3] = math.random(6)
 		end
 	end
