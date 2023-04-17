a5eb40984d9cd85e1e1ba7facbe73e2346dc4b40
AmGona <44752650+AmGona@users.noreply.github.com>
2023-04-17 00:54:27 -0700

    [Ball Race] Fixed time format

--- gamemodes/ballrace/gamemode/round.lua
+++ gamemodes/ballrace/gamemode/round.lua
@@ -513,10 +513,10 @@ function GM:PlayerComplete(ply)
 
 	placement = placement + 1
 
-	local finishTime = string.FormattedTime( ply.RaceTime )
+	local finishTime = string.FormattedTime( ply.RaceTime, "%02i.%02i.%02i" )
 
 	ply:SetNet( "CompletedRank", placement )
-	ply:SetNet( "CompletedTime", tostring(string.format("%0.2i", math.floor(finishTime.s)).."."..string.format("%0.2i", math.floor(finishTime.ms) ) ) )
+	ply:SetNet( "CompletedTime", string.Replace( finishTime, "00.", "" ) )
 
 	--PrintMessage( HUD_PRINTTALK, ply:Name()..' got '..PlacementPostfix(placement)..' place! Time Completed: '..string.FormattedTime(ply.RaceTime, "%02i:%02i:%02i")..'.' )
 	self:ColorNotifyAll( "LVL "..level.." #"..placement.." "..ply:Name().." | "..ply:GetNet( "CompletedTime" ) )
