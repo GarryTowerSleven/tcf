e6505bf3e1f35d34c7f6b4a646ed3cb74c35a816
Ashton <gmodtowerproject@gmail.com>
2023-04-21 23:40:19 -0500

    [Lobby] Jetpack power fix

--- gamemodes/gmodtower/gamemode/loadables/scoreboard3/cl_settings.lua
+++ gamemodes/gmodtower/gamemode/loadables/scoreboard3/cl_settings.lua
@@ -716,7 +716,7 @@ function SETTINGSCATEGORYTAB:CreateContents( tab )
 	if tabname == "VIP" then
 		self:Header( "VIP" )
 		self:CheckBox( "Enable Glow For Yourself", "gmt_vip_enableglow" )
-		self:Slider( "Unlimited Jetpack Power", "gmt_vip_jetpackpower", 1, 2, 1 )
+		self:Slider( "Jetpack Power", "gmt_vip_jetpackpower", 1, 2, 1 )
 		//self:CheckBox( VIPCanvas, "Draw Jetpack For Other Players", "gmt_jetpackvipdraw" )
 	end
 
