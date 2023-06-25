2ad38b9bfda006cce371f25c1c5dcba53cc9aee5
Ashton <gmodtowerproject@gmail.com>
2023-06-25 00:07:15 -0500

    [Lobby] Plasma TV offset fix

--- gamemodes/gmtlobby/entities/entities/gmt_room_tv_plasma/cl_init.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_room_tv_plasma/cl_init.lua
@@ -2,7 +2,7 @@ include('shared.lua')
 
 ENT.PlayerConfig = {
 	angle = Angle(-90, 90, 0),
-	offset = Vector(6, 28.535, 35.23),
+	offset = Vector(6.1, 28.535, 35.23),
 	width = 56.5,
 	height = 32.628 --51.1 * 9/16
 }
