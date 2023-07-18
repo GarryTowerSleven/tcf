f51e68df219de13d3a7490793dc3b231709a4dbc
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-07-18 13:50:00 -0400

    [Lobby] Fix suite cleanup

--- gamemodes/gmodtower/gamemode/loadables/roomlobby1/room/reset.lua
+++ gamemodes/gmodtower/gamemode/loadables/roomlobby1/room/reset.lua
@@ -9,25 +9,27 @@ function ResetRoom( self )
 	self:UpdateRoomSaveData()
 
 	for _, ent in pairs( self:EntsInRoom() ) do
-	
-		local ItemId = self:InventorySave( ent )
-		
-		if ItemId then
-		
-			local Item = GTowerItems:CreateById( ItemId, self.Owner )
-			local ItemSlot = GTowerItems:NewItemSlot( self.Owner, "-2" )
+		if ent:GetClass() != "gmt_trunk" then
+			local ItemId = self:InventorySave( ent )
 			
-			ItemSlot:FindUnusedSlot( Item, true )
+			if ItemId then
 			
-			if ItemSlot:IsValid() && ItemSlot:Allow( Item, true ) then
+				local Item = GTowerItems:CreateById( ItemId, self.Owner )
+				local ItemSlot = GTowerItems:NewItemSlot( self.Owner, "-2" )
 				
-				ItemSlot:Set( Item )
-				ItemSlot:ItemChanged()
+				ItemSlot:FindUnusedSlot( Item, true )
 				
-				ent:Remove()
-				
+				if ItemSlot:IsValid() && ItemSlot:Allow( Item, true ) then
+					
+					ItemSlot:Set( Item )
+					ItemSlot:ItemChanged()
+					
+					ent:Remove()
+					
+				end
+					
 			end
-				
+			
 		end
 		
 	end
