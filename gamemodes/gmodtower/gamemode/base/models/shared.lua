1614df5f37c3acc4350e78043c26d88bbb2bb761
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-07-18 13:17:16 -0400

    [Global] Add James and JC models

--- gamemodes/gmodtower/gamemode/base/models/shared.lua
+++ gamemodes/gmodtower/gamemode/base/models/shared.lua
@@ -115,6 +115,8 @@ AddValidModel( "freddykruger", "models/player/freddykruger.mdl" )
 AddValidModel( "greenarrow", "models/player/greenarrow.mdl" )
 AddValidModel( "linktp", "models/player/linktp.mdl" )
 AddValidModel( "roman", "models/player/romanbellic.mdl" )
+AddValidModel( "denton", "models/player/jcplayer.mdl" )
+AddValidModel( "james", "models/player/sh/james_sunderland.mdl" )
 --AddValidModel( "ornstein", "models/nikout/darksouls2/characters/olddragonslayer.mdl" )
 
 // Remove bad playermodels
