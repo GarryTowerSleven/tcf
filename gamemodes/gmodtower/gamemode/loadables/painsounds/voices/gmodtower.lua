f911020f67b776f27e7c3d21b1ce7cf09a7c191e
LeadKiller <hl2epicfan@gmail.com>
2023-07-21 12:53:56 -0700

    [Global] CHRISS!!!!

---
+++ gamemodes/gmodtower/gamemode/loadables/painsounds/voices/gmodtower.lua
@@ -0,0 +1,21 @@
+voicelines.Add("models/player/re/chris.mdl", {
+    Pain = {
+        Small = {
+            "gmodtower/player/re/chris/pain01.wav",
+            "gmodtower/player/re/chris/pain02.wav"
+        },
+        Medium = {
+            "gmodtower/player/re/chris/pain03.wav",
+            "gmodtower/player/re/chris/pain04.wav",
+        },
+        Large = {
+            "gmodtower/player/re/chris/pain05.wav",
+            "gmodtower/player/re/chris/pain016.wav"
+        }
+    },
+    Death = {
+        "gmodtower/player/re/chris/death01.wav"
+    }, // We'll typically reuse Large Pain.
+    Taunts = {
+    }
+})
