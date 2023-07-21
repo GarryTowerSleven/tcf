a6925a1437cbd37f94f1d456af55b6712c9fa997
LeadKiller <hl2epicfan@gmail.com>
2023-07-21 11:32:50 -0700

    [Global] Finish most of voicelines

--- gamemodes/gmodtower/gamemode/loadables/painsounds/sh_pain.lua
+++ gamemodes/gmodtower/gamemode/loadables/painsounds/sh_pain.lua
@@ -1,8 +1,8 @@
-
+voicelines = {}
 playerSounds = {}
 local mdls = {}
 
-function addSounds(mdl, tbl)
+function voiceines.Add(mdl, tbl)
     playerSounds[mdl] = tbl
 end
 
@@ -55,6 +55,10 @@ function getSounds(mdl, type)
     end
 end
 
+function voicelines.SetFemale(mdl)
+    mdls[mdl] = true
+end
+
 function isFemaleDefault(mdl)
     if mdls[mdl] then
         return mdls[mdl] == 1
@@ -63,7 +67,7 @@ function isFemaleDefault(mdl)
     mdls[mdl] = string.find(mdl, "female") and 1 or 0
 end
 
-function emitSound(ent, snd)
+function voiceline.Emit(ent, snd)
     if istable(snd) then
 
     elseif isfunction(snd) then
@@ -74,127 +78,3 @@ function emitSound(ent, snd)
         ent:EmitSound(snd, 60, 100, 1, CHAN_VOICE)
     end
 end
-
-local zombie = {
-    Pain = {"Zombie.Pain"},
-    Death = {"Zombie.Die"},
-    Taunts = {"Zombie.Idle"}
-}
-
-// This isn't used, since we're reimplementing HEV Voice Lines...
-local hev = {
-    Pain = {
-        Small = {"S:HEV_DMG4"},
-        Medium = {"S:HEV_DMG5"},
-        Large = {"S:HEV_DMG5"}
-    },
-    Death = {"S:HEV_DEAD0", "S:HEV_DEAD1"}
-}
-
-local cs = {
-    Pain = {
-        "hostage/hpain/hpain1.wav",
-        "hostage/hpain/hpain2.wav",
-        "hostage/hpain/hpain3.wav",
-        "hostage/hpain/hpain4.wav",
-        "hostage/hpain/hpain5.wav",
-        "hostage/hpain/hpain6.wav"
-    },
-    Death = {
-        "player/death1.wav",
-        "player/death2.wav",
-        "player/death3.wav",
-        "player/death4.wav",
-        "player/death5.wav",
-        "player/death6.wav"
-    }
-}
-
-local cs_bot = {
-    Pain = {
-        "bot/ouch.wav",
-        "bot/ow.wav",
-        "bot/aah.wav",
-        "bot/oh.wav"
-    },
-    Death = {
-        "bot/pain2.wav",
-        "bot/pain4.wav",
-        "bot/pain5.wav",
-        "bot/pain8.wav",
-        "bot/pain9.wav"
-    }
-}
-
-local combine = {
-    Pain = {
-            "S:METROPOLICE_PAIN0",
-            "S:METROPOLICE_PAIN1",
-            "S:METROPOLICE_PAIN2"
-    },
-    Death = {
-        "S:METROPOLICE_DIE0",
-        "S:METROPOLICE_DIE1",
-        "S:METROPOLICE_DIE2"
-    }
-}
-
-
-local combine2 = {
-    Pain = {
-            "S:COMBINE_PAIN0",
-            "S:COMBINE_PAIN1",
-            "S:COMBINE_PAIN2"
-    },
-    Death = {
-        "S:COMBINE_DIE0",
-        "S:COMBINE_DIE1",
-        "S:COMBINE_DIE2"
-    }
-}
-
-
-addSounds("default_male", {
-    Pain = {
-        Small = {
-            "vo/npc/male01/ow01.wav",
-            "vo/npc/male01/ow02.wav"
-        },
-        Medium = {
-            "vo/npc/male01/pain01.wav",
-            "vo/npc/male01/pain02.wav",
-            "vo/npc/male01/pain03.wav",
-            "vo/npc/male01/pain04.wav",
-            "vo/npc/male01/pain05.wav",
-            "vo/npc/male01/pain06.wav"
-        },
-        Large = {
-            "vo/npc/male01/pain07.wav",
-            "vo/npc/male01/pain08.wav",
-            "vo/npc/male01/pain09.wav"
-        }
-    },
-    Death = {}, // We'll typically reuse Large Pain.
-    Taunts = {}
-})
-
-addSounds("models/player/charple.mdl", zombie)
-addSounds("models/player/corpse1.mdl", zombie) // DarkRP Hobo
-addSounds("models/player/skeleton.mdl", zombie) // TODO: do some funny bone noises
-addSounds("models/player/zombie_classic.mdl", zombie)
-addSounds("models/player/zombie_fast.mdl", zombie)
-addSounds("models/player/zombie_soldier.mdl", zombie)
-
-addSounds("models/player/arctic.mdl", cs_bot)
-addSounds("models/player/gasmask.mdl", cs_bot)
-addSounds("models/player/leet.mdl", cs_bot)
-addSounds("models/player/phoenix.mdl", cs_bot)
-addSounds("models/player/riot.mdl", cs_bot)
-addSounds("models/player/swat.mdl", cs_bot)
-addSounds("models/player/urban.mdl", cs_bot)
-
-mdls["models/player/alyx.mdl"] = 1
-mdls["models/player/mossman.mdl"] = 1
-mdls["models/player/mossman_arctic.mdl"] = 1
-mdls["models/player/p2_chell.mdl"] = 1
-mdls["models/player/police_fem.mdl"] = 1
