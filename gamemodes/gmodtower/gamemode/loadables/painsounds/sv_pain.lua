f979b1309ace1dcc91aee1d9a21d12a070495ff4
LeadKiller <hl2epicfan@gmail.com>
2023-07-22 11:50:17 -0700

    [Global] Death Voicelines

--- gamemodes/gmodtower/gamemode/loadables/painsounds/sv_pain.lua
+++ gamemodes/gmodtower/gamemode/loadables/painsounds/sv_pain.lua
@@ -1,7 +1,16 @@
 hook.Add("EntityTakeDamage", "PainSounds", function(e, dmg)
     if e:IsPlayer() then
         local d = dmg:GetDamage()
+        if d >= e:Health() then return end
         local t = d > 40 and "Large" or d > 20 and "Medium" or "Small"
         voicelines.Emit(e, "Pain," .. t)
     end
+end)
+
+hook.Add("PlayerDeath", "PainSounds", function(ply)
+    voicelines.Emit(ply, "Death")
+end)
+
+hook.Add("PlayerDeathSound", "PainSounds", function()
+    return true
 end)
