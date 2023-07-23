4460fce142fc0757b625b5830978a1a14b436cd0
LeadKiller <hl2epicfan@gmail.com>
2023-07-23 05:40:30 -0700

    [Global] Wesker Eye Trail

--- gamemodes/gmodtower/gamemode/extensions/cl_model.lua
+++ gamemodes/gmodtower/gamemode/extensions/cl_model.lua
@@ -71,3 +71,53 @@ function meta:SetPlayerProperties( ply )
 	end
 
 end
+
+local limit = 4
+local glow = Material("cable/redlaser")
+local glow2 = Material("sprites/glow04_noz")
+
+hook.Add("PostPlayerDraw", "Wesker", function(ply)
+	if ply:GetModel() ~= "models/player/re/albert_wesker_overcoat_pm.mdl" then return end
+	local pos = ply:GetAttachment(1)
+	ply.Eyes = ply.Eyes or {{}, {}}
+
+	if !ply.LastEyes or ply.LastEyes < SysTime() then
+		table.remove(ply.Eyes[1], limit + 1)
+		table.remove(ply.Eyes[2], limit + 1)
+
+		if ply:GetVelocity():Length2D() < 64 or math.abs(math.NormalizeAngle(ply:EyeAngles().y - ply:GetVelocity():Angle().y)) > 90 then
+			table.insert(ply.Eyes[1], 1, pos.Pos + pos.Ang:Forward() * 0 + pos.Ang:Up() * 0.5 + pos.Ang:Right() * 1)
+			table.insert(ply.Eyes[2], 1, pos.Pos + pos.Ang:Forward() * 0 + pos.Ang:Up() * 0.5 - pos.Ang:Right() * 1.25)
+		else
+			table.remove(ply.Eyes[1], 1)
+			table.remove(ply.Eyes[2], 1)
+		end
+
+		ply.LastEyes = SysTime() + 0.01
+	end
+
+	render.SetMaterial(glow)
+	glow:SetVector("$color2", Vector(1, 0.9, 0))
+
+	render.StartBeam(#ply.Eyes[1])
+	local color = Color(124, 72, 0, 100)
+	for _, e in ipairs(ply.Eyes[1]) do
+		print(e)
+		render.AddBeam(e, 1 - (_ / (#ply.Eyes[1])), 1 - (_ / (#ply.Eyes[1])), color)
+	end
+	render.EndBeam()
+
+	render.StartBeam(#ply.Eyes[2])
+	for _, e in ipairs(ply.Eyes[2]) do
+		print(e)
+		render.AddBeam(e, 1 - (_ / (#ply.Eyes[2])), 1 - (_ / (#ply.Eyes[2])), color)
+	end
+	render.EndBeam()
+
+	render.SetMaterial(glow2)
+	color = Color(255, 150, 0)
+	color.a = 8
+	render.DrawSprite(pos.Pos + pos.Ang:Forward() * 0.4 + pos.Ang:Up() * 0.5 + pos.Ang:Right() * 1, 2, 2, color)
+	render.DrawSprite(pos.Pos + pos.Ang:Forward() * 0.4 + pos.Ang:Up() * 0.5 + pos.Ang:Right() * -1.25, 2, 2, color)
+	//render.DrawBeam()
+end)
