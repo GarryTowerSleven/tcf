8515a562d2f8543f124a289a06793b4ca0c6c093
LeadKiller <hl2epicfan@gmail.com>
2023-07-23 06:32:26 -0700

    [Global] Changed hook names

--- gamemodes/gmodtower/gamemode/loadables/painsounds/cl_init.lua
+++ gamemodes/gmodtower/gamemode/loadables/painsounds/cl_init.lua
@@ -1,4 +1,4 @@
-hook.Add("PlayerBindPress", "a", function(_, b)
+hook.Add("PlayerBindPress", "Voicelines", function(_, b)
     if b == "gmod_undo" then
         net.Start("Taunt")
         net.SendToServer()
