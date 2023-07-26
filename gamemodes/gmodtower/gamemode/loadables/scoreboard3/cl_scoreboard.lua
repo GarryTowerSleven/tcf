5dc090025ce7b282a78432fa224432a0f9cb1998
LeadKiller <hl2epicfan@gmail.com>
2023-07-25 22:54:39 -0700

    [Lobby] Scoreboard Models update

--- gamemodes/gmodtower/gamemode/loadables/scoreboard3/cl_scoreboard.lua
+++ gamemodes/gmodtower/gamemode/loadables/scoreboard3/cl_scoreboard.lua
@@ -727,6 +727,7 @@ end
 
 vgui.Register( "ScoreboardPlayerAvatar", PLAYERAVATAR )
 
+local g = Material("vgui/gradient-r")
 
 PLAYER = {}
 
@@ -836,27 +837,40 @@ function PLAYER:PerformLayout()
 	if ActionVisible then
 		self.Model:MoveLeftOf(self.Action, 5)
 	else
-		self.Model:AlignRight()
+		self.Model:AlignRight(-5)
 	end
 
 	if self.Model:GetModel() ~= self.Player:GetModel() then
 		self.Model:SetModel(self.Player:GetModel())
 
+		self.Setup = false
+
 		self.Model.LayoutEntity = function(self, ent)
 			local att = ent:LookupAttachment("eyes")
 			if att > 0 then
 				local att = ent:GetAttachment(att)
-				self:SetLookAt(att.Pos - Vector(0, 0, 0))
-				self:SetCamPos(att.Pos - att.Ang:Right() * 128 + att.Ang:Forward() * 128 - Vector(0, 0, 0))
-				ent:SetSequence(ent:LookupSequence("idle_all_01"))
+				local t = PositionSpawnIcon(ent, ent:GetPos(), false)
+				//self:SetLookAt(att.Pos - Vector(0, 0, 0))
+				//self:SetCamPos(att.Pos - att.Ang:Right() * 128 + att.Ang:Forward() * 64 - Vector(0, 0, 0))
+				self:SetCamPos(t.origin - att.Ang:Right() * 64)
+				self:SetLookAt(att.Pos)
+				self:SetFOV(t.fov * 1.4)
+				ent:SetSequence(ent:LookupSequence("taunt_laugh_base"))
 			end
 
-			self.LayoutEntity = function() end
+			if !self.Setup then
+				self.Setup = true
+				timer.Simple(1, function()
+					if IsValid(self) then
+						self.LayoutEntity = function() end
+					end
+				end)
+			end
 		end
 
 	end
 
-	self.Model:SetFOV(8)
+	// self.Model:SetFOV(8)
 end
 
 function PLAYER:Update()
@@ -976,6 +990,10 @@ function PLAYER:Paint( w, h )
 		end
 
 	end
+
+	surface.SetMaterial(g)
+	surface.SetDrawColor(Color(0, 0, 0, 200))
+	surface.DrawTexturedRect(w - h * 2, 0, h * 2, h)
 
 	// Name
 	local nameX = self.Avatar.x + self.Avatar:GetWide() + 5
