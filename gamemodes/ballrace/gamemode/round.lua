7866deefbf917f9fbbc7e7614be7cf677d147966
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-07-27 20:44:21 -0400

    Revert "[Ballrace] Bonus round is always 30 seconds"
    This reverts commit d61a6cdb86c02c0a5df78f7e27f7faf1e935b744.

--- gamemodes/ballrace/gamemode/round.lua
+++ gamemodes/ballrace/gamemode/round.lua
@@ -241,7 +241,7 @@ function GM:StartRound()
 	if LateSpawn != nil && (LateSpawn:GetName() == 'bonus_start' || LateSpawn:GetName() == 'bns_start' || LateSpawn:GetName() == 'bonus') then
 		music.Play( 1, MUSIC_BONUS )
 		NextRoundState = STATE_PLAYINGBONUS
-		NextRoundTime = 30
+		NextRoundTime = ( self.DefaultLevelTime/2 )
 	else
 		music.Play( 1, MUSIC_LEVEL )
 		NextRoundState = STATE_PLAYING
