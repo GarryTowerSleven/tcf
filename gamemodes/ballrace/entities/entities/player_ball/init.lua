5c558b3481aa0db5c0f376ca8f938987233e4e11
LeadKiller <hl2epicfan@gmail.com>
2023-07-31 12:17:04 -0700

    [Ballrace] Prediction

--- gamemodes/ballrace/entities/entities/player_ball/init.lua
+++ gamemodes/ballrace/entities/entities/player_ball/init.lua
@@ -12,6 +12,7 @@ function ENT:SphereInit(r)
 	phys:SetBuoyancyRatio(0.5)
 
 	self:StartMotionController()
+	phys:EnableMotion(true)
 end
 
 function ENT:Initialize()
@@ -29,122 +30,30 @@ function ENT:Initialize()
 	self:DrawShadow( false )
 
 	self:AddEFlags( EFL_FORCE_CHECK_TRANSMIT )
-	self.links = {}
 end
 
 function ENT:Think()
-	self:GetPhysicsObject():Wake()
-end
-
-function ENT:PhysicsCollide( data, physobj )
-	if data.Speed > 100 && data.DeltaTime >= 1 && self:GetModelScale() == 1 then
-		local edata = EffectData()
-		edata:SetOrigin(data.HitPos)
-		edata:SetNormal(data.HitNormal * -1)
-		util.Effect("ball_hit", edata)
-	end
-end
-
-function ENT:PhysicsSimulate( phys, deltatime )
-	local ply = self:GetOwner()
-
-	if !IsValid(ply) then return SIM_NOTHING end
-
-	if !ply:Alive() then return end
-	local vMove = Vector(0,0,0)
-	local vAngle = Vector(0,0,0)
-	local aEyes = ply:EyeAngles()
-
-	local mass = phys:GetMass()
-	local velocity = phys:GetVelocity()
-	local anglevel = phys:GetAngleVelocity()
-
-	if velocity:Length() >= 3000 then
-		ply:SetAchievement( ACHIEVEMENTS.BRSPEEDSTER, 1 )
-	end
-
-	if ( ply:KeyDown( IN_FORWARD ) ) then vMove = vMove + aEyes:Forward() end
-	if ( ply:KeyDown( IN_BACK) ) then vMove = vMove - aEyes:Forward() end
-	if ( ply:KeyDown( IN_MOVELEFT ) ) then vMove = vMove - aEyes:Right() end
-	if ( ply:KeyDown( IN_MOVERIGHT ) ) then vMove = vMove + aEyes:Right() end
-
-	vMove.z = 0;
-
-	if vMove:Length() > 0 then
-		local dir = vMove:GetNormal()
-
-		local dot = 1 - dir:Dot(velocity:GetNormal())
-
-		vMove = dir * mass * 20000 * deltatime
-
-		vMove = vMove + (vMove * dot)
-
-	elseif math.abs(velocity.z) <= 10 then
-
-		velocity.z = 0
-
-		local length = velocity:Length()
-		velocity:Normalize()
-
-		vMove = velocity * -length * 200 * (1 - deltatime)
-	end
-
-	vMove = self:ApplyRepellers(phys, deltatime, vMove)
-
-	return vAngle, vMove, SIM_GLOBAL_FORCE
-
-end
-
-//local repellers
-function ENT:ApplyRepellers(phys, deltatime, vMove)
-	//if !repellers then
-		local repellers = ents.FindByClass("repeller")
-	//end
-
-	for k,v in pairs(repellers) do
-		local diff = (phys:GetPos() - v:LocalToWorld(v:OBBCenter()))
-		local dist = diff:Length()
-		local scaledist = dist / 100
-
-		if dist < v.Radius then
-			if !self.links[v] then
-				self:LinkWithRepeller(v)
-			end
-
-			diff:Normalize()
-
-			if v.Repel <= 0 then
-				diff = diff * -1
-			end
-
-			local accel = v.Power * phys:GetMass() / (scaledist * scaledist)
-			vMove = vMove + (diff * accel)
-
-		elseif self.links[v] then
-			self:LinkWithRepeller(v)
-		end
-	end
-
-	return vMove
-end
-
-function ENT:LinkWithRepeller(repel)
-	if self.links[repel] then
-		self.links[repel] = false
-	else
-		self.links[repel] = true
-	end
-
-	umsg.Start("br_electrify")
-
-	umsg.Entity(repel)
-	umsg.Entity(self)
-
-	umsg.Bool(self.links[repel])
-
-	umsg.End()
+	self:PhysWake()
+	self:GetPhysicsObject():EnableGravity(false)
 end
 
 function ENT:UpdateTransmitState()
 	return TRANSMIT_ALWAYS
 end
+
+util.AddNetworkString("test")
+
+net.Receive("test", function(_, ply)
+	local pos = net.ReadVector()
+	local ball = ply:GetBall()
+
+
+
+	if pos:Distance(ball:GetPos()) > 64 then
+		net.Start("test")
+		net.WriteVector(ball:GetPos())
+		net.Send(ply)
+	else
+		ball:GetPhysicsObject():SetPos(pos)
+	end
+end)
--- gamemodes/ballrace/gamemode/cl_init.lua
+++ gamemodes/ballrace/gamemode/cl_init.lua
@@ -391,6 +391,8 @@ function GM:CalcView( ply, origin, angles, fov )
 		return view
 	end
 
+	ball = ball.Ball
+
 	view.origin, dist = ply:CameraTrace(ball, dist, angles)
 	view.origin = view.origin + Vector(0, 0, 8)
 
