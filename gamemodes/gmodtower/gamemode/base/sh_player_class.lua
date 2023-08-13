06ff635a56c95088070a42fa0cc38115b30c63df
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-13 02:11:10 -0400

    [Global] Playercolor in gamemodes

--- gamemodes/gmodtower/gamemode/base/sh_player_class.lua
+++ gamemodes/gmodtower/gamemode/base/sh_player_class.lua
@@ -1,6 +1,14 @@
 AddCSLuaFile()
 DEFINE_BASECLASS( "player_default" )
 
+if ( CLIENT ) then
+
+	CreateConVar( "cl_playercolor", "0.24 0.34 0.41", { FCVAR_ARCHIVE, FCVAR_USERINFO }, "The value is a Vector - so between 0-1 - not between 0-255" )
+	CreateConVar( "cl_playerbodygroups", "0", { FCVAR_ARCHIVE, FCVAR_USERINFO, FCVAR_DONTRECORD }, "The bodygroups to use, if the model has any" )
+	-- CreateConVar( "cl_weaponcolor", "0.30 1.80 2.10", { FCVAR_ARCHIVE, FCVAR_USERINFO, FCVAR_DONTRECORD }, "The value is a Vector - so between 0-1 - not between 0-255" )
+
+end
+
 local PLAYER = {}
 
 PLAYER.DisplayName = "GMT Player"
