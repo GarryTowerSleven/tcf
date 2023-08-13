cdb430b93255bfbca36d47583b865fd8f0756987
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-13 15:56:50 -0400

    Functionality 2.61

--- gamemodes/gmtlobby/entities/entities/gmt_scarydoor/init.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_scarydoor/init.lua
@@ -13,17 +13,22 @@ end
 function ENT:Use(ply)
 	if ply:IsPlayer() then
 
-		if ply:GetNWBool("Outside") then
-			self:EmitSound( "doors/door1_stop.wav", 75, 100, 1, CHAN_AUTO ) 
-			timer.Simple(0.25, function()
-				ply:SetNWBool("Outside", false)
-				ply:ConCommand( "gmt_updateplayermodel" )
-				ply:ConCommand( "gmt_updateplayercolor" )
-				ply:ResetSpeeds()
-				
-				ply:SafeTeleport( Vector(928, -1472, 168 ))
-				ply:SetAchievement( ACHIEVEMENTS.SMOOTHDETECTIVE, 1 )
-			end)
+		if ply.DoorDelay == nil then ply.DoorDelay = CurTime() end
+
+		if ply.DoorDelay <= CurTime() then
+			ply.DoorDelay = CurTime() + 3
+			self:EmitSound( "doors/door1_stop.wav", 75, 100, 1, CHAN_AUTO )
+			if ply:GetNWBool("Outside") then
+				timer.Simple(0.25, function()
+					ply:SetNWBool("Outside", false)
+					ply:ConCommand( "gmt_updateplayermodel" )
+					ply:ConCommand( "gmt_updateplayercolor" )
+					ply:ResetSpeeds()
+
+					ply:SafeTeleport( Vector(928, -1472, 168 ))
+					ply:SetAchievement( ACHIEVEMENTS.SMOOTHDETECTIVE, 1 )
+				end)
+			end
 		end
 
 	end
