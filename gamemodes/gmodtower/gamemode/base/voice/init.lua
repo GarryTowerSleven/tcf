73372f40b6e3ae0df15bdd2148436e22b4626c5c
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-15 17:23:24 -0400

    [Lobby] Bind voice chat in the dev hq to keep quiet

--- gamemodes/gmodtower/gamemode/base/voice/init.lua
+++ gamemodes/gmodtower/gamemode/base/voice/init.lua
@@ -2,10 +2,13 @@ hook.Add("PlayerCanHearPlayersVoice", "GMTAdminAllTalk", function(listener, talk
 
     if not IsLobby then return true end
 
+	if Location.Find(talker:GetPos()) == 7 && Location.Find(listener:GetPos()) != 7 then -- bind to devhq
+		return false
+	end
+	
     if listener:GetPos():WithinDistance( talker:GetPos(), 1024 ) then
         return true, true
     end
 
-    return false
-
+	return false
 end)
