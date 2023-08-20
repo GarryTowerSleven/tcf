0bfbe952703eb4b09fecc4d47ab6070fd65394dd
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-20 11:05:41 -0400

    [Lobby] Allow room jukeboxes to have an infinite duration

--- gamemodes/gmodtower/gamemode/loadables/mediaplayer/players/jukebox/init.lua
+++ gamemodes/gmodtower/gamemode/loadables/mediaplayer/players/jukebox/init.lua
@@ -13,6 +13,8 @@ function MEDIAPLAYER:Init()
     self._VoteManager = MediaPlayer.VoteManager:New( self )
     self._VoteskipManager = MediaPlayer.VoteskipManager:New( self )
 
+	self._IsRoom = false // Temp, we should make a proper player for all of this
+	
     self:on( "mediaChanged", function( media )
         self._VoteskipManager:Clear()
 
@@ -27,13 +29,14 @@ end
 function MEDIAPLAYER:Think()
 	BaseClass.Think( self )
 
-	if ( not self:GetOwner() ) then
+	if ( not self:GetOwner() ) then // Temp, we should make a proper player for all of this
 		local roomid = Location.GetSuiteID( self:GetLocation() )
 		if ( roomid > 0 ) then
 			local owner = GTowerRooms.GetOwner( roomid )
 			if ( not owner ) then return end
 
 			self:SetOwner( owner )
+			self._IsRoom = true // Temp, we should make a proper player for all of this
 		end
 	end
 end
@@ -83,7 +86,7 @@ function MEDIAPLAYER:ShouldQueueMedia( media )
     if ( not duration ) then return false, T( "Theater_RequestFailed" ) end
 
     // check duration
-    if ( duration > self._MaxDuration ) then return false, T( "TheaterTooLong" ) end
+    if ( duration > self._MaxDuration ) && self._IsRoom == false then return false, T( "TheaterTooLong" ) end  // Temp, we should make a proper player for all of this
 
     for _, v in ipairs( self._Queue ) do
         if ( v.IsOwner && v:IsOwner( owner ) ) then
