c4a0f9785901ca92d6d2a61d4cbe971793d98174
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-23 23:17:41 -0400

    [Lobby] Painsound delay

--- gamemodes/gmodtower/gamemode/loadables/painsounds/sv_pain.lua
+++ gamemodes/gmodtower/gamemode/loadables/painsounds/sv_pain.lua
@@ -1,13 +1,18 @@
 hook.Add("EntityTakeDamage", "PainSounds", function(e, dmg)
     if e:IsPlayer() then
-        local d = dmg:GetDamage()
-        if d >= e:Health() then return end
-        local t = d > 40 and "Large" or d > 20 and "Medium" or "Small"
-        voicelines.Emit(e, "Pain," .. t)
+		e.LastPainTime = e.LastPainTime or CurTime()
+		if e.LastPainTime <= CurTime() then
+			local d = dmg:GetDamage()
+			if d >= e:Health() then return end
+			local t = d > 40 and "Large" or d > 20 and "Medium" or "Small"
+			voicelines.Emit(e, "Pain," .. t)
+			e.LastPainTime = CurTime() + 1
+		end
     end
 end)
 
 hook.Add("PlayerDeath", "PainSounds", function(ply)
+	ply.LastPainTime = nil
     voicelines.Emit(ply, "Death")
 end)
 
