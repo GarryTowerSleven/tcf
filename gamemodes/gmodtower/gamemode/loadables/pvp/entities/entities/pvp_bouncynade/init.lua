4f1bb0f1f070cf0e81df902e9e9c241b3a0a2476
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-25 11:10:40 -0400

    [PVPBattle] Rework/Buff the rubber grenade slightly

--- gamemodes/gmodtower/gamemode/loadables/pvp/entities/entities/pvp_bouncynade/init.lua
+++ gamemodes/gmodtower/gamemode/loadables/pvp/entities/entities/pvp_bouncynade/init.lua
@@ -4,17 +4,19 @@ AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 
 ENT.BlastRange	= 300
-ENT.BlastDmg	= 50
+ENT.BlastDmg	= 80
+ENT.Bounce = 2.25
 
 function ENT:PhysicsCollide( data, phys )
 	local ent = data.HitEntity
-
+	self.Bounce = self.Bounce * .75
+	
 	if data.Speed > 80 && data.DeltaTime > 0.2 then
 		self:EmitSound(self.BlastSound, 80)
 	end
-
+	
 	local LastSpeed = data.OurOldVelocity:Length()
 	local NewVelocity = phys:GetVelocity():GetNormalized()
 	LastSpeed = math.max( NewVelocity:Length(), LastSpeed )
-	phys:SetVelocity( NewVelocity * LastSpeed * 1.1 )
+	phys:SetVelocity( NewVelocity * LastSpeed * self.Bounce )
 end
