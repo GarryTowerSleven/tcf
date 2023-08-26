047f5d86b9deb8b2af173ff53873e3e55fadbd08
cattslmao <cattttttslmao@gmail.com>
2023-08-25 21:32:54 -0500

    [Global] FORGOT

--- gamemodes/gmodtower/entities/entities/gmt_cosmeticbase/init.lua
+++ gamemodes/gmodtower/entities/entities/gmt_cosmeticbase/init.lua
@@ -14,3 +14,18 @@ function ENT:Initialize()
 	self:AddToEquipment()
 
 end
+
+function ENT:ReParent()
+	self:SetParent(NULL)
+	self:SetParent(self:GetOwner())
+end
+
+local PlayerMeta = FindMetaTable("Player")
+
+function PlayerMeta:ResetEquipmentAfterVehicle()
+	if !IsValid(self) || !self.CosmeticEquipment then return end
+
+	for k,v in pairs(self.CosmeticEquipment) do
+		v:ReParent()
+	end
+end
