5b9dc056fe59e4ed25f880d7634267482e663a67
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-08-30 18:18:56 -0400

    [Lobby] Working mikuclock

--- gamemodes/gmtlobby/entities/entities/gmt_mikuclock/init.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_mikuclock/init.lua
@@ -8,10 +8,58 @@ function ENT:Initialize()
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
+	self:SetUseType(SIMPLE_USE)
 	self:DrawShadow(true)
 	
 	local phys = self:GetPhysicsObject()
 	if IsValid(phys) then
 		phys:Wake()
 	end
+
+	self.Playing = false
+	self.LastSong = 0
+	
+	self:Precache()
+end
+
+function ENT:Use(ply)
+	if self.Playing then return end
+
+	self.Playing = true
+
+	local song = self:PickSong()
+	self.Song = CreateSound( self, song )
+	self.Song:PlayEx( .45, 100 )
+	timer.Simple(10, function()
+		if IsValid(self) then 
+			self.Playing = false
+			if self.Song then self.Song:FadeOut(1) end
+		end
+	end)
+end
+
+function ENT:OnRemove()
+	print("TEST")
+	if self.Song then self.Song:Stop() end
+end
+function ENT:Think()
+	if !self.Playing then return end
+	
+	self:EmitNotes()		
+	self:NextThink(CurTime() + 2)
+end
+
+function ENT:PickSong()
+	local song = self.Songs[math.random(#self.Songs)]
+	if self.LastSong == song then return self:PickSong() end
+	self.LastSong = song
+	return song
+end
+
+function ENT:EmitNotes()
+	local edata = EffectData()
+	edata:SetOrigin(self:GetPos())
+	edata:SetEntity(self)
+
+	util.Effect("musicnotes", edata, true, true)
 end
--- gamemodes/gmtlobby/entities/entities/gmt_mikuclock/shared.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_mikuclock/shared.lua
@@ -3,3 +3,21 @@ ENT.Type 	= "anim"
 ENT.Base 	= "base_anim"
 
 ENT.Model	= Model("models/gmod_tower/mikuclock.mdl")
+
+ENT.Songs = {"GModTower/lobby/mikuclock/mikuclock_song|.mp3", 18} --Second value is the amount of songs, starting from 01.
+
+function ENT:Precache()
+	local tbl, str, strSong = {}, string.Explode("|", self.Songs[1])
+	
+	local function convertNum(num)
+		if num >= 0 and num <= 9 then return tostring(0 .. num) end
+		return num
+	end
+	
+	for i = 1, self.Songs[2] do
+		strSong = str[1] .. convertNum(i) .. str[2]
+		table.insert(tbl, strSong)
+	end
+	
+	self.Songs = tbl
+end
