80b50f81af848298adb4e108a1eaae10a0137b54
LeadKiller <hl2epicfan@gmail.com>
2023-09-02 12:34:10 -0700

    [Lobby] New Alcohol

--- gamemodes/gmodtower/gamemode/base/inventory/items/empty_bottle.lua
+++ gamemodes/gmodtower/gamemode/base/inventory/items/empty_bottle.lua
@@ -1,12 +1,14 @@
 ---------------------------------
 ITEM.Name = T("empty_bottle")
 ITEM.Description = T("empty_bottle_desc")
-ITEM.Model = "models/props_junk/garbage_glassbottle001a.mdl"
+ITEM.Model = "models/gmod_tower/boozebottle.mdl"
+ITEM.ClassName = "alcohol_bottle"
 ITEM.UniqueInventory = false
 ITEM.DrawModel = true
 ITEM.AllowEntBreak = true
 ITEM.MoveSound = "glass"
 ITEM.AllowBarDrop = true
+ITEM.ModelColor = Color(254, 254, 254)
 
 ITEM.StoreId = GTowerItems.BarStoreId
 ITEM.StorePrice = 3
--- gamemodes/gmtlobby/entities/entities/alcohol_bottle/shared.lua
+++ gamemodes/gmtlobby/entities/entities/alcohol_bottle/shared.lua
@@ -14,11 +14,41 @@ function ENT:Initialize()
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetUseType(SIMPLE_USE)
 
+	timer.Simple(0, function()
+		if self:GetColor().r == 254 then
+			self:GetPhysicsObject():EnableMotion(true)
+		end
+	end)
+
 	self.Drank = false
+end
+
+function ENT:PhysicsCollide(data)
+	if data.Speed < 400 then return end
+
+	if self.Ply then
+		self.Ply:SetAchievement( ACHIEVEMENTS.DOMESTICABUSE, 1 )
+		self.Ply:AddAchievement( ACHIEVEMENTS.TRASHCOMPACTOR, 1 )
+	end
+
+	local fx = EffectData()
+	fx:SetOrigin(self:GetPos())
+	util.Effect("AntlionGib", fx, true, true)
+	util.Decal("BeerSplash", self:GetPos(), self:GetPos() + data.HitNormal * 24, self)
+	self:EmitSound("GlassBottle.Break")
+	self:Remove()
 end
 
 function ENT:Use(ply)
 	if CLIENT || self.Drank then return end
+
+	if self:GetColor().r == 254 then
+		if !self:IsPlayerHolding() then
+			ply:PickupObject(self)
+			self.Ply = ply
+		end
+		return
+	end
 
 	self.Drank = true
 	ply:Drink()
@@ -36,4 +66,26 @@ function ENT:Use(ply)
 	if IsValid(ent:GetPhysicsObject()) then
 		ent:GetPhysicsObject():Wake()
 	end
+end
+
+local mat = Material("models/debug/debugwhite")
+
+function ENT:Draw()
+	if self:GetColor().r != 254 then
+	self:SetModelScale(0.95)
+	render.SetBlend(0.9)
+	render.SetColorModulation(0.718, 0.541, 0.125)
+	render.ModelMaterialOverride(mat)
+	render.EnableClipping(true)
+	local n = -self:GetUp()
+	render.PushCustomClipPlane(n, n:Dot(self:WorldSpaceCenter() + self:GetUp() * 6))
+	self:DrawModel()
+	render.PopCustomClipPlane()
+	render.EnableClipping(false)
+	end
+	self:SetModelScale(1)
+	render.SetBlend(0.8)
+	render.SetColorModulation(1, 1, 1)
+	render.ModelMaterialOverride()
+	self:DrawModel()
 end
