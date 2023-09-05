8a7c7183bd5f6258f29738847972dfed29d3a9db
LeadKiller <hl2epicfan@gmail.com>
2023-09-05 03:49:23 -0700

    [Lobby] Emotion on death

--- gamemodes/gmodtower/gamemode/loadables/emotion/init.lua
+++ gamemodes/gmodtower/gamemode/loadables/emotion/init.lua
@@ -10,7 +10,7 @@ module( "Emotions", package.seeall )
 
 function meta:SetEmotion( enum, time, fear )
 	self.DesiredEmotion = enum
-	self.LastEmotion = CurTime() + ( time or 30 )
+	self.LastEmotionTime = CurTime() + ( time or 30 )
 	if fear then return end
 	hook.GetTable()["PlayerThink"]["Emotions"]( self )
 end
@@ -22,14 +22,15 @@ end)
 hook.Add("PlayerThink", "Emotions", function(ply)
 	local default = ply.AFK and EMOTION_SLEEPY || ( ply:GetNet( "BAL" ) || 0 ) > 0 && EMOTION_WASTED || ply.LastKeyPress and ply.LastKeyPress + 30 < CurTime() && EMOTION_BORED || EMOTION_HAPPY
 
-	if ply.LastDefault != default || ply.LastEmotion < CurTime() then
+	if ply.LastDefault != default && ply.LastEmotionTime < CurTime() then
 		ply:SetEmotion( default, nil, true )
-		ply.LastDefault = default
+		ply.LastDefault = defaultP
 	end
 
 	if ply.LastEmotion != ply.DesiredEmotion then
 		ply:SetNet( "Emotion", ply.DesiredEmotion )
 		ply.LastEmotion = ply.DesiredEmotion
+		ply.LastDefault = nil
 	end
 end)
 
@@ -41,4 +42,8 @@ hook.Add("EntityTakeDamage", "Emotion", function(ply, dmg)
 	if ply:IsPlayer() then
 		ply:SetEmotion( EMOTION_PAIN, 2 )
 	end
+end)
+
+hook.Add("PlayerDeath", "Emotion", function(ply)
+	ply:SetEmotion( EMOTION_SLEEPY, math.huge )
 end)
