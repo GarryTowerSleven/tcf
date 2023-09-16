34163d8a8da92c1013e000159d131e2922d99a34
Bumpy226 <robintron226@gmail.com>
2023-09-17 00:43:11 +0200

    [Ballrace] End bonus when all bananas have been eaten.

--- gamemodes/ballrace/entities/entities/banana/init.lua
+++ gamemodes/ballrace/entities/entities/banana/init.lua
@@ -31,14 +31,16 @@ function ENT:Touch(ent)
 	if ent:GetClass() != "player_ball" || self.Eaten then return end
 
 	ent:GetOwner():AddFrags(self.PointValue)
-
 	ent:GetOwner():EmitSound( self.EatSound )
 	ent:GetOwner():AddAchievement( ACHIEVEMENTS.BRBANANABINGER, 1 )
+
 	local effectdata = EffectData()
 		effectdata:SetOrigin( self:GetPos() + Vector( 0, 0, 20 ) )
 	util.Effect( "bananaeat", effectdata, true, true )
 
 	self.Eaten = true
+
+	self:CheckAllEatenInLevel()
 
 	self:Remove()
 end
@@ -47,4 +49,18 @@ function ENT:KeyValue(key, value)
 	if key == "points" then
 		self.PointValue = tonumber(value)
 	end
+end
+
+function ENT:CheckAllEatenInLevel()
+	local bananasLeft = -1 -- -1 because we don't count the one we just ate
+
+	for k, v in pairs(ents.FindByClass("banana")) do
+		if v:TestPVS(self) then
+			bananasLeft = bananasLeft + 1
+		end
+	end
+
+	GAMEMODE.BananasLeft = bananasLeft
+	
+	hook.Call("BananaEaten", GAMEMODE, self)
 end
