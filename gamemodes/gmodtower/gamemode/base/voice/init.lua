78645a6f85062d6496137b32ee52643eb7626f24
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-09-15 22:45:09 -0400

    [Lobby] Huge fucking perf gain

--- gamemodes/gmodtower/gamemode/base/voice/init.lua
+++ gamemodes/gmodtower/gamemode/base/voice/init.lua
@@ -4,9 +4,16 @@ hook.Add("PlayerCanHearPlayersVoice", "GMTAdminAllTalk", function(listener, talk
 	
     if not IsLobby then return true end
 
-	if Location.Find(talker:GetPos()) == 7 && Location.Find(listener:GetPos()) != 7 then -- bind to devhq
-		return false
+	if talker._VoiceLockedToLocation or listener._VoiceLockedToLocation then
+		if talker:Location() == listener:Location() then
+			return true, true
+		end
 	end
+	
+	//Let's find a better way to do this? I hope this raises the perf
+	/*if Location.Find(talker:GetPos()) == 7 && Location.Find(listener:GetPos()) != 7 then -- bind to devhq
+		return false
+	end*/
 	
     if listener:GetPos():WithinDistance( talker:GetPos(), 1024 ) then
         return true, true
@@ -14,3 +21,14 @@ hook.Add("PlayerCanHearPlayersVoice", "GMTAdminAllTalk", function(listener, talk
 
 	return false
 end)
+
+
+hook.Add( "Location", "LocationVoiceLock", function( ply, loc ) 
+
+	if Location.Is( loc, "Dev HQ" ) then
+		ply._VoiceLockedToLocation = true
+	else
+		ply._VoiceLockedToLocation = false
+	end
+
+end )
