0e21127b5381159b33b08c5d49e780469f0473ae
Bumpy226 <robintron226@gmail.com>
2023-09-25 18:42:52 +0200

    [Lobby] Removed unused money NPC netmessage.

--- gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_money/cl_init.lua
+++ gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_money/cl_init.lua
@@ -1,31 +1,5 @@
 include('shared.lua')
 
-net.Receive( "gmt_npc_money", function()
-
-	Derma_StringRequest(
-		"Who wanted money?",
-		"Set the amount of GMC you want to get. (Max: 100,000)",
-		"0",
-		function( txt )
-
-			net.Start( "gmt_npc_money" )
-				net.WriteUInt( tonumber( txt ) or 0, 32 )
-			net.SendToServer()
-
-		end,
-        function()
-
-			net.Start( "gmt_npc_money" )
-				net.WriteUInt( 0, 32 ) // just to let the server know we're not interested
-			net.SendToServer()
-            
-        end,
-		"GIVE ME THAT DOSH!",
-		"Nah nevermind I'm good"
-	)
-
-end )
-
 /*hook.Add( "OnEntityCreated", "VIPBones", function( ent ) 
 
 	if ent:GetClass() == "gmt_npc_vip" then
--- gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_money/init.lua
+++ gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_money/init.lua
@@ -19,25 +19,5 @@ function ENT:Use( ply )
 	/*if ( ply._MoneyCooldown && ply._MoneyCooldown > CurTime() ) then
 		ply:Msg2( "Timeout." )
 		return
-	end
-
-	net.Start( "gmt_npc_money" )
-	net.Send( ply )*/
+	end*/
 end
-
-/*net.Receive( "gmt_npc_money", function( len, ply )
-	local npc = ents.FindByClass( "gmt_npc_money" )[1]
-	if ( not IsValid( npc ) ) then return end
-
-	if ( IsValid( ply ) && ply:GetPos():Distance( npc:GetPos() ) < 512 ) then
-		if ( ply._MoneyCooldown && ply._MoneyCooldown > CurTime() ) then return end
-
-		local amount = math.Clamp( net.ReadUInt( 32 ) or 0, 0, 100000 )
-		if ( amount < 1 ) then return end
-
-		ply:AddMoney( amount )
-		ply._MoneyCooldown = CurTime() + npc.CoolDown
-	end
-end )*/
-
-util.AddNetworkString( "gmt_npc_money" )
