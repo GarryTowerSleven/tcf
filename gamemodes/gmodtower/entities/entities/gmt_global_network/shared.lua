cdc224b4bae4b2b71b7d9af2ebb0e9927277bb2b
cattslmao <cattttttslmao@gmail.com>
2023-10-01 21:08:04 -0500

    [Global] Globalnet Fix

--- gamemodes/gmodtower/entities/entities/gmt_global_network/shared.lua
+++ gamemodes/gmodtower/entities/entities/gmt_global_network/shared.lua
@@ -1,7 +1,15 @@
+AddCSLuaFile()
+
 ENT.Base		= "base_anim"
 ENT.Type		= "anim"
 
 function ENT:Initialize()
+
+	if SERVER then
+
+		self:AddEFlags( EFL_FORCE_CHECK_TRANSMIT )
+
+	end
 
 	globalnet.InitializeOn( self )
 
@@ -10,3 +18,10 @@ end
 /*function ENT:SetupDataTables()
 	globalnet.InitializeOn( self )
 end*/
+
+function ENT:UpdateTransmitState()
+	return TRANSMIT_ALWAYS
+end
+
+function ENT:Draw()
+end
