70d71e69bfda9574b95d53d24340b3a8e8d8185d
cattslmao <cattttttslmao@gmail.com>
2023-10-02 19:56:29 -0500

    [Global] Serverside PlayerSpawnClient

--- gamemodes/gmodtower/gamemode/extensions/sh_playerhooks.lua
+++ gamemodes/gmodtower/gamemode/extensions/sh_playerhooks.lua
@@ -1,52 +1,83 @@
 local netStringName = "gmt_playerhook_initpost"
-local validEntityPollTimeout = 30 -- Time out in 30 seconds if the entity doesn't become valid
+local validEntityPollTimeout = 30
 
 if SERVER then
-	util.AddNetworkString(netStringName)
+    
+    util.AddNetworkString( netStringName )
 
-	local function ClientCreated( ply )
+    local queue = queue or {}
+
+    local function ClientCreated( ply )
+
 		if ply._ClientCreated then return end
+
+        if queue[ ply ] then
+            queue[ ply ] = nil
+        end
+
 		ply._ClientCreated = true
+
 		-- Call the hook on the server to inform a client is initialized
 		hook.Call( "PlayerSpawnClient", GAMEMODE, ply )
 
 		-- Send it back to all the clients so we can do a hook THANG
-		net.Start(netStringName)
-			net.WriteInt(ply:EntIndex(), 10)
+		net.Start( netStringName )
+			net.WriteInt( ply:EntIndex(), 10 )
 		net.Broadcast()
+
 	end
 
-	-- Receiving from this means
-	net.Receive(netStringName, function(len, ply)
-		ClientCreated(ply)
+    hook.Add( "PlayerInitialSpawn", "GMTPlayerCreated", function( ply )
+        if ply:IsBot() then
+            ClientCreated( ply )
+
+            return
+        end
+
+        queue[ ply ] = true
+    end )
+
+    hook.Add( "SetupMove", "GMTPlayerCreated", function( ply, _, cmd )
+        if queue[ ply ] and not cmd:IsForced() then
+            queue[ ply ] = nil
+
+            ClientCreated( ply )
+        end
+    end )
+
+	net.Receive( netStringName, function( len, ply )
+        if queue[ ply ] then
+            queue[ ply ] = nil
+        end
+
+		ClientCreated( ply )
 	end )
 
-	-- Quick fix for bots
-	hook.Add("PlayerInitialSpawn", "GMTBotPlayerCreated", function(ply)
-		if not ply:IsBot() then return end
-
-		ClientCreated(ply)
-	end )
-end 
+end
 
 if CLIENT then
-
-	-- Called when our world is ready, we're loaded and dandy
-	hook.Add("InitPostEntity", "GMTPlayerCreated", function()
-		net.Start(netStringName)
+    
+    -- Called when our world is ready, we're loaded and dandy
+	hook.Add( "InitPostEntity", "GMTPlayerCreated", function()
+		net.Start( netStringName )
 		net.SendToServer()
 	end )
 
-	local function ClientCreated( ply )
+    local function ClientCreated( ply )
+
 		if ply._ClientCreated then return end
+
 		ply._ClientCreated = true
+
 		hook.Call( "PlayerSpawnClient", GAMEMODE, ply )
+
 	end
 
-	-- We actually recieve the player before they are even valid
+    -- We actually recieve the player before they are even valid
 	-- Hold a queue that we poll until they are valid
 	local ReceiveQueue = {}
-	net.Receive(netStringName, function()	
+	net.Receive( netStringName, function()	
+
 		local entindex = net.ReadInt(10)
 		local ply = Entity(entindex)
 
@@ -57,10 +88,12 @@ if CLIENT then
 
 		-- Now call the hook for the clients in the house
 		ClientCreated( ply )
+
 	end )
 
 	-- Just poll for when (if) they become valid
-	hook.Add("Think", "GMTPlayerSpawnClientPoll", function()
+	hook.Add( "Think", "GMTPlayerSpawnClientPoll", function()
+
 		for k, v in pairs( ReceiveQueue ) do
 			local ply = Entity(v.Index)
 
@@ -75,5 +108,7 @@ if CLIENT then
 				ReceiveQueue[k] = nil 
 			end
 		end
+
 	end )
+
 end
