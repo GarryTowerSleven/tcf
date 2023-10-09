9a4488717ee7214140bf49cf319eb660ba7b0f59
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-10-08 22:59:42 -0400

    [Lobby] OnUse for turtle secrets

--- gamemodes/gmtlobby/entities/entities/gmt_gateway/shared.lua
+++ gamemodes/gmtlobby/entities/entities/gmt_gateway/shared.lua
@@ -6,3 +6,5 @@ ENT.Model			= "models/props/de_tides/vending_turtle.mdl"
 ENT.Correct = Sound( "GModTower/lobby/firework/firework_launch.wav" )
 
 ENT.False = Sound( "GModTower/lobby/firework/firework_explode.wav" )
+
+ENT.OnUse = "???"
