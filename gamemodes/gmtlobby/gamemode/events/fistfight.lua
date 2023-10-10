518993aa4e0d653788bce0add0f02a43734824a9
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-10-10 02:39:13 -0400

    skymsg mistake

--- gamemodes/gmtlobby/gamemode/events/fistfight.lua
+++ gamemodes/gmtlobby/gamemode/events/fistfight.lua
@@ -28,4 +28,93 @@ EVENT.Locations = {
 
 EVENT.MoneyOnKill = 5
 
+function EVENT:Start()
+
+	if CLIENT then
+		return
+	end
+    
+    local data, loc = table.Random( self.Locations )
+
+    self.ActiveLocation = loc
+    self.ActiveSpawns = data.spawns
+	
+	MsgT( "MiniBattleGameStart", string.lower( self.BattleType ), Location.GetFriendlyName( loc ) )
+
+    for _, v in ipairs( Location.GetPlayersInLocation( self.ActiveLocation ) ) do
+        self:AddParticipant( v )
+    end
+
+    if self.HookGroup then
+
+        self.HookGroup:DisableHooks()
+        self.HookGroup = nil
+        
+    end
+
+    self.HookGroup = hookgroup.New()
+
+    self.HookGroup:Add( "Location", "Event_" .. self.Name, function( ply, loc )
+
+        if loc == self.ActiveLocation then
+            
+            self:AddParticipant( ply )
+
+        else
+        
+            self:RemoveParticipant( ply )
+
+        end
+
+    end )
+
+    self.HookGroup:Add( "PlayerDeath", "Event_" .. self.Name, function( victim, inflictor, attacker )
+
+        self:PlayerDeath( victim, attacker )
+
+    end )
+
+    self.HookGroup:Add( "PlayerSpawn", "Event_" .. self.Name, function( ply )
+
+        self:PlayerSpawn( ply )
+
+    end )
+
+    self.HookGroup:Add( "PlayerShouldTakeDamage", "Event_" .. self.Name, function( ply, attacker )
+
+        if self.Participants[ ply ] then
+            return true
+        end
+
+    end )
+
+    self.HookGroup:EnableHooks()
+	
+end
+
+function EVENT:End()
+
+    if CLIENT then
+		return
+	end
+
+    if self.TotalMoney > 0 then
+        MsgT( "MiniTotal", string.FormatNumber( self.TotalMoney ) )
+    end
+
+    self.ActiveLocation = nil
+    self.ActiveSpawns = nil
+
+    self.Participants = {}
+    self.TotalMoney = 0
+
+    if self.HookGroup then
+
+        self.HookGroup:DisableHooks()
+        self.HookGroup = nil
+
+    end
+	
+end
+
 minievent.Register( "FistFight", EVENT )
