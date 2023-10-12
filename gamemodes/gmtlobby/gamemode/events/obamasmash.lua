7946af4434af03b681532700a84e97d23c161705
Ashton <75055761+evil-scout@users.noreply.github.com>
2023-10-12 02:29:59 -0400

    yes

--- gamemodes/gmtlobby/gamemode/events/obamasmash.lua
+++ gamemodes/gmtlobby/gamemode/events/obamasmash.lua
@@ -123,7 +123,7 @@ function EVENT:SpawnerThink()
     local data = self.Locations[ self.ActiveLocation ]
     local limit = data.limit or 25
 
-	limit = math.min( math.max( math.Round( limit * ( smashers * .1 ) ), 5 ), limit ) // extreme dynamic obama calculations
+	limit = math.Clamp( math.Round( limit * ( smashers * .1 ) ), 5, limit ) // extreme dynamic obama calculations
 	
     if table.Count( self.Entities ) < limit then
 
