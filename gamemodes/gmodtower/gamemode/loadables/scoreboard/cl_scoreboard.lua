dd12f3672afc0e7a4ecb8fce5bb3ddc2bb8581ae
LeadKiller <hl2epicfan@gmail.com>
2023-12-11 05:51:52 -0800

    [Global] what??????????

--- gamemodes/gmodtower/gamemode/loadables/scoreboard/cl_scoreboard.lua
+++ gamemodes/gmodtower/gamemode/loadables/scoreboard/cl_scoreboard.lua
@@ -1866,7 +1866,7 @@ hook.Add( "GMTScoreboardShow", "RestoreAvatarDHTML", function()
 	timer.Remove( "DisableAvatarDHTML" )
 
     for k, v in pairs( PLAYERS.AnimatedAvatars ) do
-		if IsValid( v ) and IsValid( v.Ply ) then
+		if IsValid( v ) and IsValid( v.Ply ) && !v:IsBot() then
 			v:SetupAnimatedAvatar( v.Ply, v:GetSize() )
 		end
 	end
