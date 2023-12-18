292e4020ba1ddcd6829781e8c9887129fc80d307
LeadKiller <hl2epicfan@gmail.com>
2023-12-18 01:37:48 -0800

    [Lobby] VIP Offset

--- gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_vip/cl_init.lua
+++ gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_vip/cl_init.lua
@@ -2,7 +2,7 @@ include('shared.lua')
 
 ENT.NPCExpression = "blank"
 
-ENT.Offset = -4
+ENT.Offset = -3.5
 ENT.Hat = "models/gmod_tower/aviators.mdl"
 ENT.HatOffset = {
 	Pos = Vector( -1.25, 0, -3 ),
