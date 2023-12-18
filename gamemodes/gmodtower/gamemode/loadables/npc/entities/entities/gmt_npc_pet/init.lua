e3dc6a768cf42f76ce74d3a0131d451d94057458
LeadKiller <hl2epicfan@gmail.com>
2023-12-17 21:31:20 -0800

    [Lobby] Give the pets store NPC a pet

--- gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_pet/init.lua
+++ gamemodes/gmodtower/gamemode/loadables/npc/entities/entities/gmt_npc_pet/init.lua
@@ -1,3 +1,19 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include('shared.lua')
+
+function ENT:Think()
+
+    self.BaseClass.Think( self )
+
+    if !IsValid( self.Pet ) then
+
+        self.Pet = ents.Create( "gmt_pet" )
+        self.Pet:SetPos( self:GetPos() )
+        self.Pet:SetOwner( self )
+        self.Pet:SetPetName( "Meowlen!" )
+        self.Pet:Spawn()
+
+    end
+
+end
